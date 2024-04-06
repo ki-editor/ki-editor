665a59e20b29b1c76a8ba6d39333fe02ff748894
wongjiahau <hou32hou@gmail.com>
2024-04-06 20:22:15 +0800

    chore: revert changes to build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    // shared::grammar::fetch_grammars();
-    // shared::grammar::build_grammars();
+    shared::grammar::fetch_grammars();
+    shared::grammar::build_grammars();
 
     println!("cargo:rerun-if-changed=build.rs");
 }
