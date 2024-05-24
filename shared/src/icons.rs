b286cc9b2f53d9e2d6dbd68bde91672d3e8a3a66
WJH <hou32hou@gmail.com>
2024-05-24 21:20:16 +0800

    refactor(emoji icons): remove emojis containing ZWJ (#198)

--- shared/src/icons.rs
+++ shared/src/icons.rs
@@ -15,8 +15,20 @@ pub struct IconsConfig {
     pub completion: HashMap<String, String>,
 }
 
+const ZERO_WIDTH_JOINER: char = '\u{200D}';
 pub fn get_icon_config() -> &'static IconsConfig {
     ICON_CONFIG.get_or_init(|| {
-        serde_json::from_str(include_str!("../../contrib/emoji-icon-theme.json")).unwrap()
+        let result: IconsConfig =
+            serde_json::from_str(include_str!("../../contrib/emoji-icon-theme.json")).unwrap();
+        result.file_extensions.values().for_each(check_for_zwj);
+        result.file_names.values().for_each(check_for_zwj);
+        result.completion.values().for_each(check_for_zwj);
+        result
     })
+}
+
+fn check_for_zwj(value: &String) {
+    if value.chars().any(|c| c == ZERO_WIDTH_JOINER) {
+        panic!("The value {} contains ZWJ (Zero-width joiner). Joined characters are not supported for now.", value);
+    }
 }
