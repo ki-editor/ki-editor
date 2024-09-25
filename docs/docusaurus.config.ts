a9975dfbbfbf1bf0c9c586b39d81a2101d231c2f
wongjiahau <hou32hou@gmail.com>
2024-09-25 20:39:44 +0800

    refactor: rename docu/ to docs/

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,136 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Ki Editor",
+  tagline: "Multi-cursor structural editor",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://ki-editor.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "ki-editor", // Usually your GitHub org/user name.
+  projectName: "ki-editor", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/ki-editor/ki-editor/tree/master",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/ki-editor/ki-editor/tree/master",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/tree-seal-script.svg",
+    navbar: {
+      title: "Ki Editor",
+      logo: {
+        alt: "Ki Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          position: "left",
+          label: "Tutorial",
+          to: "/docs/tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/ki-editor/ki-editor",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "/docs/introduction",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Matrix",
+              href: "https://matrix.to/#/#ki-editor:matrix.org",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/ki-editor/ki-editor",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Ki Editor. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,70 @@
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "First-class syntax node interaction",
+    Svg: require("@site/static/img/syntax-node.svg").default,
+    description: (
+      <>
+        Bridge the gap between coding intent and action: manipulate syntax
+        structures directly, avoiding mouse or keyboard gymnastics.
+      </>
+    ),
+  },
+  {
+    title: "Multiple cursors",
+    Svg: require("@site/static/img/multiple-cursors.svg").default,
+    description: (
+      <>
+        Amplify your coding efficiency: wield multiple cursors for parallel
+        syntax node operations, revolutionizing bulk edits and refactoring.
+      </>
+    ),
+  },
+  {
+    title: "Redefine modal editing",
+    Svg: require("@site/static/img/handdrawn-cube.svg").default,
+    description: (
+      <>
+        Selection Modes standardize movements across words, lines, syntax nodes,
+        and more, offering unprecedented flexibility and consistency.
+      </>
+    ),
+  },
+];
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
