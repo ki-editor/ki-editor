362486438b277675f14ce5e4f21f635ba0edfc75
Jeremy Cowgar <jeremy@cowgar.com>
2025-01-02 19:51:42 -0500

    chore(test): mark Editor::dirty as a test only function

--- src/components/editor.rs
+++ src/components/editor.rs
@@ -498,6 +498,7 @@ pub(crate) enum Movement {
 
 impl Editor {
     /// Returns if the editor is currently dirty or not.
+    #[test]
     pub(crate) fn dirty(&self) -> bool {
         self.dirty
     }
