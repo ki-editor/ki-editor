7809e99b4ea3bf8c4e127f983f3999030b7ce6f3
kxhxn <hilarykodji@gmail.com>
2025-02-15 03:07:11 +0000

    feat(languages/idris): Add support for Idris

--- shared/src/languages.rs
+++ shared/src/languages.rs
@@ -21,6 +21,7 @@ pub const LANGUAGES: &[&Language] = &[
     &hare(),
     &heex(),
     &html(),
+    &idris(),
     &javascript(),
     &javascript_react(),
     &json(),
@@ -358,6 +359,24 @@ const fn html() -> Language {
             id: "html",
             url: "https://github.com/tree-sitter/tree-sitter-html",
             commit: "master",
+            subpath: None,
+        }),
+        ..Language::new()
+    }
+}
+
+const fn idris() -> Language {
+    Language {
+        extensions: &["idr", "lidr", "ipkg"],
+        lsp_command: Some(LspCommand {
+            command: Command("idris2-lsp", &[]),
+            ..LspCommand::default()
+        }),
+        lsp_language_id: Some(LanguageId::new("idris")),
+        tree_sitter_grammar_config: Some(GrammarConfig {
+            id: "idris",
+            url: "https://github.com/kayhide/tree-sitter-idris",
+            commit: "main",
             subpath: None,
         }),
         ..Language::new()
