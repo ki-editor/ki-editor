8d9d1261cffd244ea036f64ae3249fd545cdfe05
morfert <100343258+morfert@users.noreply.github.com>
2025-03-25 12:40:27 +0500

    feat(system_clipboard): support Wayland (#559)
    Also added a secondary package to the flake that builds which builds with the `wayland` feature.
    
    Fixes #558.

--- flake.nix
+++ flake.nix
@@ -7,25 +7,30 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs =
-    { self
-    , nixpkgs
-    , crane
-    , flake-utils
-    , rust-overlay
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
     }:
     let
       # Normalizes Rust version string to ensure it has a patch version.
       # - Returns version as-is if it matches "X.Y.Z" format (e.g. "1.83.0")
       # - Appends ".0" if version matches "X.Y" format (e.g. "1.83" -> "1.83.0")
       # - Throws an error for any other format
-      normalizeRustVersion = version:
+      normalizeRustVersion =
+        version:
         let
           majorMinorPatch = builtins.match "([0-9]+)[.]([0-9]+)[.]([0-9]+)" version;
           majorMinor = builtins.match "([0-9]+)[.]([0-9]+)" version;
         in
-        if majorMinorPatch != null then version
-        else if majorMinor != null then version + ".0"
-        else throw "Invalid Rust version format: ${version}. Expected format: X.Y.Z or X.Y";
+        if majorMinorPatch != null then
+          version
+        else if majorMinor != null then
+          version + ".0"
+        else
+          throw "Invalid Rust version format: ${version}. Expected format: X.Y.Z or X.Y";
     in
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -38,37 +43,56 @@
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         rustVersion = normalizeRustVersion cargoToml.package.rust-version;
         rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default.override {
-          extensions = [ "rust-src" "rust-analyzer" ];
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
         };
         commonArgs = {
           src = ./.;
           strictDeps = true;
           doCheck = false;
-          buildInputs = with pkgs; [
-            openssl
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-            darwin.apple_sdk.frameworks.CoreServices
-            darwin.apple_sdk.frameworks.CoreFoundation
-            darwin.apple_sdk.frameworks.CoreGraphics
-            darwin.apple_sdk.frameworks.AppKit
-          ];
+          buildInputs =
+            with pkgs;
+            [
+              openssl
+            ]
+            ++ lib.optionals stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+              darwin.apple_sdk.frameworks.CoreServices
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.CoreGraphics
+              darwin.apple_sdk.frameworks.AppKit
+            ];
           nativeBuildInputs = with pkgs; [
             pkg-config
             git
             rustToolchain
           ];
         };
+        waylandCommonArgs = (
+          commonArgs
+          // {
+            cargoExtraArgs = "--features wayland";
+          }
+        );
         ki-editor = craneLib.buildPackage (
           commonArgs
           // {
             cargoArtifacts = craneLib.buildDepsOnly commonArgs;
           }
         );
+        ki-editor-wayland = craneLib.buildPackage (
+          waylandCommonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly waylandCommonArgs;
+          }
+        );
       in
       {
         packages.default = ki-editor;
+        packages.ki-editor-wayland = ki-editor-wayland;
         devShells.default = craneLib.devShell {
           packages = [ pkgs.rust-analyzer ];
         };
