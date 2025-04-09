ki-editor/ki-editor

commit 3b8deb79677b71427af27aaddd9ebcc9b6808d83

perf(selection_mode): use `get_current_selection_by_cursor` instead of `iter` for some selection modes (#562)

src/buffer.rs
@@ -1,6 +1,7 @@
 use crate::history::History;
 use crate::lsp::diagnostic::Diagnostic;
 use crate::quickfix_list::QuickfixListItem;
+use crate::selection::Selection;
 use crate::selection_mode::naming_convention_agnostic::NamingConventionAgnostic;
 use crate::syntax_highlight::SyntaxHighlightRequestBatchId;
 use crate::{
@@ -9,7 +10,7 @@ use crate::{
     context::{LocalSearchConfig, LocalSearchConfigMode},
     edit::{Action, ActionGroup, Edit, EditTransaction},
     position::Position,
-    selection::{CharIndex, Selection, SelectionSet},
+    selection::{CharIndex, SelectionSet},
     selection_mode::{AstGrep, ByteRange},
     syntax_highlight::{HighlightedSpan, HighlightedSpans},
     utils::find_previous,
@@ -1063,6 +1064,16 @@ impl Buffer {
         } else {
             Ok(None)
         }
+    }
+
+    pub(crate) fn line_to_char_range(&self, line: usize) -> anyhow::Result<CharIndexRange> {
+        let start = self.line_to_char(line)?;
+        let end = self.line_to_char(line + 1)?;
+        Ok((start..end).into())
+    }
+
+    pub(crate) fn char(&self, cursor_char_index: CharIndex) -> char {
+        self.rope.char(cursor_char_index.0)
     }
 }
 

commit 6bb36373ff00a398a55ff4f630463e92e86b64d1

fix(selection_mode/line): buggy vertical movement (#572)
This is fixed by introducing a `next_char_index` and `prev_char_index` in the PositionBasedSelectionMode trait.

And then Line selection mode will override these two methods and return the first char index of the next or previous line.

These bugs are introduced by 3b8deb79677b71427af27aaddd9ebcc9b6808d83.

src/buffer.rs
@@ -348,7 +348,16 @@ impl Buffer {
     }
 
     pub(crate) fn len_lines(&self) -> usize {
-        self.rope.len_lines()
+        // Need to minus 1 if last character is a newline.
+        // For some reason, Rope::len_lines will return an extra line
+        // if the last character is a newline.
+        let deduction =
+            if let Some('\n') = self.rope.get_char(self.rope.len_chars().saturating_sub(1)) {
+                1
+            } else {
+                0
+            };
+        self.rope.len_lines().saturating_sub(deduction)
     }
 
     pub(crate) fn char_to_line(&self, char_index: CharIndex) -> anyhow::Result<usize> {
@@ -1072,8 +1081,10 @@ impl Buffer {
         Ok((start..end).into())
     }
 
-    pub(crate) fn char(&self, cursor_char_index: CharIndex) -> char {
-        self.rope.char(cursor_char_index.0)
+    pub(crate) fn char(&self, cursor_char_index: CharIndex) -> anyhow::Result<char> {
+        self.rope
+            .get_char(cursor_char_index.0)
+            .ok_or_else(|| anyhow::anyhow!("Unable to get char at {cursor_char_index:?}"))
     }
 }
 

src/components/editor.rs
@@ -3322,16 +3322,20 @@ impl Editor {
 
                     let delete_range = buffer.line_range_to_full_char_index_range(line_range)?;
 
-                    let select_range = Selection::get_selection_(
-                        &buffer,
-                        &selection.clone().collapsed_to_anchor_range(&Direction::End),
-                        &self.selection_set.mode,
-                        &Movement::Down,
-                        &self.cursor_direction,
-                        context,
-                    )?
-                    .map(|result| result.selection.range())
-                    .unwrap_or_else(|| selection.range());
+                    let get_selection = |movement: Movement| {
+                        Selection::get_selection_(
+                            &buffer,
+                            &selection.clone().collapsed_to_anchor_range(&Direction::End),
+                            &self.selection_set.mode,
+                            &movement,
+                            &self.cursor_direction,
+                            context,
+                        )
+                    };
+                    let select_range = get_selection(Movement::Down)?
+                        .or_else(|| get_selection(Movement::Up).ok().flatten())
+                        .map(|result| result.selection.range())
+                        .unwrap_or_else(|| selection.range());
 
                     Ok([
                         ActionGroup::new(

src/position.rs
@@ -65,6 +65,13 @@ impl Position {
             column: self.column + column,
         }
     }
+
+    pub(crate) fn set_column(self, new_column: usize) -> Position {
+        Position {
+            column: new_column,
+            ..self
+        }
+    }
 }
 
 impl Ord for Position {

src/selection_mode/line_full.rs
@@ -30,7 +30,7 @@ impl PositionBasedSelectionMode for LineFull {
                 loop {
                     if index > CharIndex(buffer.len_chars().saturating_sub(1)) {
                         return Ok(None);
-                    } else if buffer.char(index).is_whitespace() {
+                    } else if buffer.char(index)?.is_whitespace() {
                         index = index + 1
                     } else {
                         break index;
@@ -74,7 +74,7 @@ impl PositionBasedSelectionMode for LineFull {
             {
                 let mut index = cursor_char_index;
                 loop {
-                    if buffer.char(index).is_whitespace() {
+                    if buffer.char(index)?.is_whitespace() {
                         if index == CharIndex(0) {
                             return Ok(None);
                         } else {

src/selection_mode/line_trimmed.rs
@@ -12,54 +12,13 @@ impl PositionBasedSelectionMode for LineTrimmed {
         &self,
         buffer: &crate::buffer::Buffer,
         cursor_char_index: crate::selection::CharIndex,
-        if_current_not_found: crate::components::editor::IfCurrentNotFound,
+        _: crate::components::editor::IfCurrentNotFound,
     ) -> anyhow::Result<Option<super::ByteRange>> {
         let last_cursor_char_index = CharIndex(buffer.len_chars());
         if cursor_char_index >= last_cursor_char_index {
             return Ok(None);
         }
-        let line_index = {
-            let current_line_index = buffer.char_to_line(cursor_char_index)?;
-            let current_char = buffer.char(cursor_char_index);
-            if !current_char.is_whitespace() {
-                current_line_index
-            } else if current_char == '\n' {
-                match if_current_not_found {
-                    IfCurrentNotFound::LookForward => current_line_index + 1,
-                    IfCurrentNotFound::LookBackward => current_line_index,
-                }
-            } else {
-                let new_char_index = match if_current_not_found {
-                    IfCurrentNotFound::LookForward => {
-                        let mut index = cursor_char_index;
-                        loop {
-                            let char = buffer.char(index);
-                            if index == last_cursor_char_index {
-                                return Ok(None);
-                            } else if !char.is_whitespace() || char == '\n' {
-                                break index;
-                            } else {
-                                index = index + 1
-                            }
-                        }
-                    }
-                    IfCurrentNotFound::LookBackward => {
-                        let mut index = cursor_char_index;
-                        loop {
-                            let char = buffer.char(index);
-                            if index == CharIndex(0) {
-                                return Ok(None);
-                            } else if !char.is_whitespace() || char == '\n' {
-                                break index;
-                            } else {
-                                index = index - 1
-                            }
-                        }
-                    }
-                };
-                buffer.char_to_line(new_char_index)?
-            }
-        };
+        let line_index = buffer.char_to_line(cursor_char_index)?;
         let Some(line) = buffer.get_line_by_line_index(line_index) else {
             return Ok(None);
         };
@@ -87,6 +46,25 @@ impl PositionBasedSelectionMode for LineTrimmed {
                 .into(),
         )?;
         Ok(Some(ByteRange::new(range)))
+    }
+
+    fn next_char_index(
+        &self,
+        params: &SelectionModeParams,
+        range: crate::char_index_range::CharIndexRange,
+    ) -> anyhow::Result<CharIndex> {
+        let line_index = params.buffer.char_to_line(range.start)?;
+        params.buffer.line_to_char(line_index + 1)
+    }
+
+    #[cfg(test)]
+    fn previous_char_index(
+        &self,
+        params: &SelectionModeParams,
+        range: crate::char_index_range::CharIndexRange,
+    ) -> anyhow::Result<CharIndex> {
+        let line_index = params.buffer.char_to_line(range.start)?;
+        params.buffer.line_to_char(line_index - 1)
     }
 
     fn left(
@@ -122,6 +100,13 @@ impl PositionBasedSelectionMode for LineTrimmed {
             })
             .transpose()?
             .flatten())
+    }
+
+    fn right(
+        &self,
+        params: &SelectionModeParams,
+    ) -> anyhow::Result<Option<crate::selection::Selection>> {
+        self.down(params)
     }
 
     fn delete_forward(
@@ -170,7 +155,7 @@ mod test_line {
 
     #[test]
     fn case_1() {
-        let buffer = Buffer::new(None, "a\n\n\nb\nc\n  hello\n  \nbye");
+        let buffer = Buffer::new(None, "a\n\n\nb  \nc\n  hello\n  \nbye\n\n");
         PositionBased(LineTrimmed).assert_all_selections(
             &buffer,
             Selection::default(),
@@ -179,11 +164,11 @@ mod test_line {
                 (2..2, ""),
                 (3..3, ""),
                 (4..5, "b"),
-                (6..7, "c"),
-                // Should not include leading whitespaces
-                (10..15, "hello"),
-                (18..18, ""),
-                (19..22, "bye"),
+                (8..9, "c"),
+                (12..17, "hello"),
+                (20..20, ""),
+                (21..24, "bye"),
+                (25..25, ""),
             ],
         );
     }

src/selection_mode/mod.rs
@@ -43,6 +43,7 @@ use crate::{
         editor::{Direction, IfCurrentNotFound, Jump, Movement, SurroundKind},
         suggestive_editor::Info,
     },
+    position::Position,
     selection::{CharIndex, Selection},
     surround::EnclosureKind,
 };
@@ -303,22 +304,7 @@ impl<T: PositionBasedSelectionMode> SelectionMode for PositionBased<T> {
         &'a self,
         params: &SelectionModeParams<'a>,
     ) -> anyhow::Result<Vec<ByteRange>> {
-        let mut cursor_char_index = CharIndex(params.buffer.len_chars() - 1);
-        let mut result = Vec::new();
-        while let Some(range) = self.get_current_selection_by_cursor(
-            params.buffer,
-            cursor_char_index,
-            IfCurrentNotFound::LookBackward,
-        )? {
-            if range.range.start == 0 || Some(&range) == result.first() {
-                result.insert(0, range);
-                break;
-            } else {
-                cursor_char_index = params.buffer.byte_to_char(range.range.start - 1)?;
-                result.insert(0, range);
-            }
-        }
-        Ok(result)
+        self.0.all_selections_gathered_inversely(params)
     }
 
     fn to_index(
@@ -743,7 +729,11 @@ pub trait PositionBasedSelectionMode {
     ) -> anyhow::Result<Option<crate::selection::Selection>> {
         self.get_current_selection_by_cursor(
             params.buffer,
-            params.current_selection.range().end,
+            params
+                .current_selection
+                .range()
+                .end
+                .min(CharIndex(params.buffer.len_chars().saturating_sub(1))),
             IfCurrentNotFound::LookForward,
         )?
         .map(|range| {
@@ -800,7 +790,10 @@ pub trait PositionBasedSelectionMode {
                 cursor_char_index,
                 IfCurrentNotFound::LookForward,
             )? {
-                cursor_char_index = params.buffer.byte_to_char(range.range.end)?;
+                cursor_char_index = self.next_char_index(
+                    params,
+                    params.buffer.byte_range_to_char_index_range(&range.range)?,
+                )?;
 
                 if Some(&range) == result.last() {
                     result.push(range);
@@ -813,6 +806,49 @@ pub trait PositionBasedSelectionMode {
             }
         }
         Ok(result)
+    }
+
+    #[cfg(test)]
+    fn all_selections_gathered_inversely<'a>(
+        &'a self,
+        params: &SelectionModeParams<'a>,
+    ) -> anyhow::Result<Vec<ByteRange>> {
+        let mut cursor_char_index = CharIndex(params.buffer.len_chars() - 1);
+        let mut result = Vec::new();
+        while let Some(range) = self.get_current_selection_by_cursor(
+            params.buffer,
+            cursor_char_index,
+            IfCurrentNotFound::LookBackward,
+        )? {
+            if range.range.start == 0 || Some(&range) == result.first() {
+                result.insert(0, range);
+                break;
+            } else {
+                cursor_char_index = self.previous_char_index(
+                    params,
+                    params.buffer.byte_range_to_char_index_range(&range.range)?,
+                )?;
+                result.insert(0, range);
+            }
+        }
+        Ok(result)
+    }
+
+    #[cfg(test)]
+    fn previous_char_index(
+        &self,
+        _: &SelectionModeParams,
+        range: CharIndexRange,
+    ) -> anyhow::Result<CharIndex> {
+        Ok(range.start - 1)
+    }
+
+    fn next_char_index(
+        &self,
+        _: &SelectionModeParams,
+        range: CharIndexRange,
+    ) -> anyhow::Result<CharIndex> {
+        Ok(range.end)
     }
 
     fn expand(&self, params: &SelectionModeParams) -> anyhow::Result<Option<ApplyMovementResult>> {
@@ -854,7 +890,17 @@ pub trait PositionBasedSelectionMode {
             current_position.line + 1
         };
 
-        let mut new_position = current_position.set_line(new_line);
+        let get_column = |new_line: usize| {
+            let line_length = buffer
+                .get_line_by_line_index(new_line)
+                .map(|slice| slice.len_chars())
+                .unwrap_or_default();
+            line_length.saturating_sub(1).min(current_position.column)
+        };
+        let mut new_position = current_position
+            .set_line(new_line)
+            .set_column(get_column(new_line));
+
         let mut new_cursor_char_index = buffer.position_to_char(new_position)?;
 
         // Define which look direction to try first and second based on movement direction
@@ -870,21 +916,23 @@ pub trait PositionBasedSelectionMode {
             )
         };
 
-        while let Some(result) =
-            self.get_current_selection_by_cursor(params.buffer, new_cursor_char_index, first_look)?
-        {
-            if buffer.byte_to_line(result.range.start)? == new_position.line {
-                return Ok(Some(
-                    (*current_selection)
-                        .clone()
-                        .set_range(buffer.byte_range_to_char_index_range(&result.range)?)
-                        .set_info(result.info),
-                ));
-            } else if let Some(result) = self.get_current_selection_by_cursor(
-                params.buffer,
-                new_cursor_char_index,
-                second_look,
-            )? {
+        loop {
+            if let Some(result) =
+                self.get_current_selection_by_cursor(buffer, new_cursor_char_index, first_look)?
+            {
+                if buffer.byte_to_line(result.range.start)? == new_position.line {
+                    return Ok(Some(
+                        (*current_selection)
+                            .clone()
+                            .set_range(buffer.byte_range_to_char_index_range(&result.range)?)
+                            .set_info(result.info),
+                    ));
+                }
+            }
+
+            if let Some(result) =
+                self.get_current_selection_by_cursor(buffer, new_cursor_char_index, second_look)?
+            {
                 if buffer.byte_to_line(result.range.start)? == new_position.line {
                     return Ok(Some(
                         (*current_selection)
@@ -896,12 +944,21 @@ pub trait PositionBasedSelectionMode {
             }
 
             // Move to next line
-            new_position.line = if is_up {
+            let new_line = if is_up {
                 new_position.line.saturating_sub(1)
             } else {
                 new_position.line + 1
             };
-            new_cursor_char_index = buffer.position_to_char(new_position)?;
+            let new_column = get_column(new_line);
+
+            new_position = Position::new(new_line, new_column);
+
+            let next_cursor_char_index = buffer.position_to_char(new_position)?;
+            if next_cursor_char_index == new_cursor_char_index {
+                break;
+            } else {
+                new_cursor_char_index = next_cursor_char_index
+            }
         }
 
         Ok(None)

src/selection_mode/token.rs
@@ -47,6 +47,7 @@ impl PositionBasedSelectionMode for Token {
         &self,
         params: &super::SelectionModeParams,
     ) -> anyhow::Result<Option<crate::selection::Selection>> {
+        // return Token { skip_symbols: !self.skip_symbols, } .left(params);
         let buffer = params.buffer;
         let current_line_index = buffer.char_to_line(params.cursor_char_index())?;
         let line_start_char_index = buffer.line_to_char(current_line_index)?;
@@ -66,6 +67,7 @@ impl PositionBasedSelectionMode for Token {
         &self,
         params: &super::SelectionModeParams,
     ) -> anyhow::Result<Option<crate::selection::Selection>> {
+        // return Token { skip_symbols: !self.skip_symbols, } .right(params);
         let buffer = params.buffer;
         let current_line_index = buffer.char_to_line(params.cursor_char_index())?;
         let next_line_index = current_line_index + 1;
@@ -111,7 +113,7 @@ impl PositionBasedSelectionMode for Token {
             let mut current = cursor_char_index;
             loop {
                 if (CharIndex(0)..=last_char_index).contains(&current) {
-                    if is_target(buffer.char(current)) {
+                    if is_target(buffer.char(current)?) {
                         break current;
                     } else {
                         match if_current_not_found {

src/selection_mode/word.rs
@@ -53,7 +53,7 @@ impl PositionBasedSelectionMode for Word {
             let mut current = cursor_char_index;
             loop {
                 if (CharIndex(0)..=last_char_index).contains(&current) {
-                    if predicate(buffer.char(current)) {
+                    if predicate(buffer.char(current)?) {
                         break current;
                     } else {
                         match if_current_not_found {
@@ -72,7 +72,7 @@ impl PositionBasedSelectionMode for Word {
             }
         };
 
-        let current_char = buffer.char(current);
+        let current_char = buffer.char(current)?;
 
         if current_char.is_whitespace()
             || (self.skip_symbols && !current_char.is_ascii_alphanumeric())
@@ -84,9 +84,9 @@ impl PositionBasedSelectionMode for Word {
             let start = {
                 let mut index = current;
                 loop {
-                    if index > CharIndex(0) && buffer.char(index - 1).is_ascii_lowercase() {
+                    if index > CharIndex(0) && buffer.char(index - 1)?.is_ascii_lowercase() {
                         index = index - 1;
-                    } else if index > CharIndex(0) && buffer.char(index - 1).is_ascii_uppercase() {
+                    } else if index > CharIndex(0) && buffer.char(index - 1)?.is_ascii_uppercase() {
                         break index - 1;
                     } else {
                         break index;
@@ -96,7 +96,7 @@ impl PositionBasedSelectionMode for Word {
             let end = {
                 let mut index = current;
                 loop {
-                    if index < last_char_index && buffer.char(index + 1).is_ascii_lowercase() {
+                    if index < last_char_index && buffer.char(index + 1)?.is_ascii_lowercase() {
                         index = index + 1;
                     } else {
                         break index;
@@ -107,14 +107,14 @@ impl PositionBasedSelectionMode for Word {
         } else if current_char.is_ascii_uppercase() {
             let start = {
                 let mut index = current;
-                if index < last_char_index && buffer.char(index + 1).is_lowercase() {
+                if index < last_char_index && buffer.char(index + 1)?.is_lowercase() {
                     index
                 } else {
                     loop {
                         if index == CharIndex(0) {
                             break index;
                         }
-                        let char = buffer.char(index - 1);
+                        let char = buffer.char(index - 1)?;
                         if char.is_ascii_uppercase() {
                             index = index - 1;
                         } else {
@@ -124,19 +124,19 @@ impl PositionBasedSelectionMode for Word {
                 }
             };
             let end = {
-                let mut previous_is_uppercase = buffer.char(current).is_ascii_uppercase();
+                let mut previous_is_uppercase = buffer.char(current)?.is_ascii_uppercase();
                 let mut index = current;
                 loop {
                     if index >= last_char_index {
                         break index;
                     }
-                    let char = buffer.char(index + 1);
+                    let char = buffer.char(index + 1)?;
                     if char.is_ascii_lowercase() {
                         previous_is_uppercase = char.is_ascii_uppercase();
                         index = index + 1;
                     } else if previous_is_uppercase && char.is_ascii_uppercase() {
                         if index < last_char_index - 1
-                            && buffer.char(index + 2).is_ascii_lowercase()
+                            && buffer.char(index + 2)?.is_ascii_lowercase()
                         {
                             break index;
                         } else {
@@ -153,7 +153,7 @@ impl PositionBasedSelectionMode for Word {
             let start = {
                 let mut index = current;
                 loop {
-                    if index > CharIndex(0) && buffer.char(index - 1).is_ascii_digit() {
+                    if index > CharIndex(0) && buffer.char(index - 1)?.is_ascii_digit() {
                         index = index - 1;
                     } else {
                         break index;
@@ -163,7 +163,7 @@ impl PositionBasedSelectionMode for Word {
             let end = {
                 let mut index = current;
                 loop {
-                    if index < last_char_index && buffer.char(index + 1).is_ascii_digit() {
+                    if index < last_char_index && buffer.char(index + 1)?.is_ascii_digit() {
                         index = index + 1;
                     } else {
                         break index;

src/components/test_editor.rs
@@ -4567,3 +4567,49 @@ hello
         ])
     })
 }
+
+#[test]
+fn move_down_from_indented_line_to_last_dedented_line() -> anyhow::Result<()> {
+    execute_test(|s| {
+        Box::new([
+            App(OpenFile {
+                path: s.main_rs(),
+                owner: BufferOwner::User,
+                focus: true,
+            }),
+            Editor(SetContent("    fo\nb".to_string())),
+            Editor(SetSelectionMode(
+                IfCurrentNotFound::LookForward,
+                SelectionMode::Line,
+            )),
+            Expect(CurrentSelectedTexts(&["fo"])),
+            Editor(MoveSelection(Down)),
+            Expect(CurrentSelectedTexts(&["b"])),
+            Editor(MoveSelection(Up)),
+            Expect(CurrentSelectedTexts(&["fo"])),
+        ])
+    })
+}
+
+#[test]
+fn delete_forward_last_dedented_lines() -> anyhow::Result<()> {
+    execute_test(|s| {
+        Box::new([
+            App(OpenFile {
+                path: s.main_rs(),
+                owner: BufferOwner::User,
+                focus: true,
+            }),
+            Editor(SetContent("    fo\nb".to_string())),
+            Editor(SetSelectionMode(
+                IfCurrentNotFound::LookForward,
+                SelectionMode::Line,
+            )),
+            Expect(CurrentSelectedTexts(&["fo"])),
+            Editor(MoveSelection(Down)),
+            Expect(CurrentSelectedTexts(&["b"])),
+            Editor(Delete(Direction::End)),
+            Expect(CurrentSelectedTexts(&["fo"])),
+        ])
+    })
+}

src/test_app.rs
@@ -145,7 +145,9 @@ pub(crate) enum ExpectKind {
     SelectionExtensionEnabled(bool),
 }
 fn log<T: std::fmt::Debug>(s: T) {
-    println!("===========\n{s:?}",);
+    if !is_ci::cached() {
+        println!("===========\n{s:?}",);
+    }
 }
 impl ExpectKind {
     fn run(&self, app: &mut App<MockFrontend>) -> anyhow::Result<()> {
@@ -210,8 +212,8 @@ impl ExpectKind {
             AppGrid(grid) => {
                 let expected = grid.to_string().trim_matches('\n').to_string();
                 let actual = app.get_screen()?.stringify().trim_matches('\n').to_string();
-                println!("expected =\n{}", expected);
-                println!("actual =\n{}", actual);
+                log(format!("expected =\n{}", expected));
+                log(format!("actual =\n{}", actual));
                 contextualize(actual, expected)
             }
             CurrentPath(path) => contextualize(app.get_current_file_path().unwrap(), path.clone()),
@@ -292,7 +294,7 @@ impl ExpectKind {
                         .rows[position.line][position.column]
                         .source;
                     if actual_style_key!=style_key {
-                        println!("Expected {position:?} to be styled as {style_key:?}, but got {actual_style_key:?}");
+                        log(format!("Expected {position:?} to be styled as {style_key:?}, but got {actual_style_key:?}"));
                     }
                     actual_style_key == style_key
                 }),
@@ -320,8 +322,8 @@ impl ExpectKind {
             QuickfixListContent(content) => {
                 let expected = app.get_quickfix_list().unwrap().render().content;
                 let actual = content.to_string();
-                println!("expected =\n{expected}");
-                println!("actual =\n{actual}");
+                log(format!("expected =\n{expected}"));
+                log(format!("actual =\n{actual}"));
                 contextualize(expected, actual)
             }
             DropdownInfosCount(expected) => {
@@ -357,7 +359,7 @@ impl ExpectKind {
                     .iter()
                     .inspect(|&span| {
                         // For debugging purposes
-                        println!("xx {span:?} {}", span.style_key.display());
+                        log(format!("xx {span:?} {}", span.style_key.display()));
                     })
                     .collect_vec()
                     .into_iter()
@@ -442,7 +444,7 @@ impl ExpectKind {
                     .map(|row| {
                         row.iter()
                             .filter(|cell| {
-                                println!("style = {:?}", cell.source);
+                                log(format!("style = {:?}", cell.source));
                                 cell.source.as_ref() == Some(style_key)
                             })
                             .count()