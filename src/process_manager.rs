b970658f8af197aeb7f27ffc5913d84bf929b17a
yontuh <jontesteiner@gmail.com>
2025-11-12 21:09:05 -0800

    Implement Drop for ToggleProcess, refactored

--- src/process_manager.rs
+++ src/process_manager.rs
@@ -31,26 +31,25 @@ impl ProcessManager {
             Ok(child) => {
                 self.running_processes.insert(key.to_string(), child);
             }
-
             Err(_e) => {}
         }
     }
     fn stop(&mut self, key: &str) {
         if let Some(mut child) = self.running_processes.remove(key) {
-            if let Err(e) = child.kill() {
-                _ = e
-            }
+            let _ = child.kill();
+        }
+    }
+
+    pub fn shutdown(&mut self) {
+        let keys: Vec<String> = self.running_processes.keys().cloned().collect();
+        for key in keys {
+            self.stop(&key);
         }
     }
 }
 
 impl Drop for ProcessManager {
     fn drop(&mut self) {
-        for (key, child) in self.running_processes.iter_mut() {
-            log::info!("Shutting down toggled process on exit: {}", key);
-            if let Err(e) = child.kill() {
-                log::error!("Failed to kill process '{}' on exit: {}", key, e);
-            }
-        }
+        self.shutdown();
     }
 }
