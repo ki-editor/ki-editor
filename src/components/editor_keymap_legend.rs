5efd9b801993cae39a42db7f5a574fd6cfc47709
Vishal <95016059+haxfn@users.noreply.github.com>
2025-11-23 18:35:32 +0530

    keymap(editor): consistency semantic quit should use Delte (#1017)
    Positional coherence, moved `Quit No Save` 
    to use the same key as `Delte`. 
    Removed `Quit Save`, moved `Save All`
    to use `Copy_` meaning.

--- src/components/editor_keymap_legend.rs
+++ src/components/editor_keymap_legend.rs
@@ -1152,13 +1152,6 @@ impl Editor {
                 Keymap::new(
                     context
                         .keyboard_layout_kind()
-                        .get_space_editor_keymap(&Meaning::QSave),
-                    "Save All Quit".to_string(),
-                    Dispatch::SaveQuitAll,
-                ),
-                Keymap::new(
-                    context
-                        .keyboard_layout_kind()
                         .get_space_editor_keymap(&Meaning::QNSav),
                     "Quit No Save".to_string(),
                     Dispatch::QuitAll,
