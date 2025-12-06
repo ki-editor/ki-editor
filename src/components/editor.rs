19bbea5e4262f787444be2211da9d9bea1a37c8e
Vishal <95016059+haxfn@users.noreply.github.com>
2025-12-06 13:08:42 +0530

    go back to `v motion v v`

--- src/components/editor.rs
+++ src/components/editor.rs
@@ -63,6 +63,7 @@ pub(crate) enum Mode {
 
 #[derive(Clone, Copy, PartialEq, Debug, Eq)]
 pub(crate) enum PriorChange {
+    EnterDeleteMode,
     EnterMultiCursorMode,
     EnableSelectionExtension,
 }
@@ -264,8 +265,7 @@ impl Component for Editor {
             MoveToLineEnd => return self.move_to_line_end(),
             SelectLine(movement) => return self.select_line(movement, context),
             Redo => return self.redo(context),
-            EnterDeleteMode => return self.enter_delete_mode(),
-            HandleDeleteMode => return self.handle_delete_mode(context),
+            DeleteOne => return self.delete_one(context),
             Change => return self.change(context),
             ChangeCut => return self.change_cut(context),
             #[cfg(test)]
@@ -1673,7 +1673,7 @@ impl Editor {
 
         let _ = self.enter_normal_mode(context);
 
-        self.apply_edit_transaction(edit_transaction, context)
+        Ok(self.apply_edit_transaction(edit_transaction, context)?)
     }
 
     /// Similar to Change in Vim, but does not copy the current selection
@@ -4042,6 +4042,7 @@ impl Editor {
     pub(crate) fn handle_prior_change(&mut self, prior_change: Option<PriorChange>) {
         if let Some(prior_change) = prior_change {
             match prior_change {
+                PriorChange::EnterDeleteMode => self.mode = Mode::Delete,
                 PriorChange::EnterMultiCursorMode => self.mode = Mode::MultiCursor,
                 PriorChange::EnableSelectionExtension => self.enable_selection_extension(),
             }
@@ -4250,19 +4251,6 @@ impl Editor {
         }
     }
 
-    fn enter_delete_mode(&mut self) -> anyhow::Result<Dispatches> {
-        self.mode = Mode::Delete;
-        Ok(Default::default())
-    }
-
-    fn handle_delete_mode(&mut self, context: &Context) -> anyhow::Result<Dispatches> {
-        match self.mode {
-            Mode::Normal => Ok(Dispatches::one(Dispatch::ToEditor(EnterDeleteMode))),
-            Mode::Delete => self.delete_one(context),
-            _ => Ok(Default::default()),
-        }
-    }
-
     fn handle_path_renamed(&mut self, source: PathBuf, destination: CanonicalizedPath) {
         let Some(path) = self.path() else { return };
         if path.to_path_buf() == &source {
@@ -4320,6 +4308,7 @@ pub(crate) enum DispatchEditor {
     SetRectangle(Rectangle),
     EnableSelectionExtension,
     DisableSelectionExtension,
+    DeleteOne,
     Change,
     ChangeCut,
     EnterInsertMode(Direction),
@@ -4342,8 +4331,6 @@ pub(crate) enum DispatchEditor {
     EnterNormalMode,
     EnterSwapMode,
     EnterReplaceMode,
-    EnterDeleteMode,
-    HandleDeleteMode,
     CursorAddToAllSelections,
     CyclePrimarySelection(Direction),
     CursorKeepPrimaryOnly,
