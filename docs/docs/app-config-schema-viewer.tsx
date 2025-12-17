2d16a172f52fd8e78b73b85d708572a5affc62a2
wongjiahau <hou32hou@gmail.com>
2025-12-17 10:28:12 +0800

    doc(app-config-schema-viewer): add title for validator

--- docs/docs/app-config-schema-viewer.tsx
+++ docs/docs/app-config-schema-viewer.tsx
@@ -3,16 +3,26 @@ import JSONSchemaEditor from "@theme/JSONSchemaEditor";
 
 import Schema from "@site/static/app_config_json_schema.json";
 
+import jsf from "json-schema-faker";
 export function AppConfigSchemaViewer() {
+    const sample = {
+        theme: "VS Code (Light)",
+        keyboard_layout: "Qwerty",
+        languages: {},
+    };
     return (
         <div style={{ display: "grid" }}>
             <JSONSchemaViewer
                 schema={Schema}
-                viewerOptions={{ showExamples: true, showExamples: true }}
+                viewerOptions={{ showExamples: true }}
             />
 
             <div style={{ display: "grid" }}>
-                <JSONSchemaEditor schema={Schema} />
+                <h2>Validator</h2>
+                <JSONSchemaEditor
+                    schema={Schema}
+                    defaultValue={JSON.stringify(sample, null, 4)}
+                />
             </div>
         </div>
     );
