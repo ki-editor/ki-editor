be5416d8c570883c878da60c50f359954431aebd
Keenan G <41458184+Riolku@users.noreply.github.com>
2025-12-30 22:04:17 -0500

    refactor(themes): embed compressed zed themes into the binary (#1114)

--- src/themes/from_zed_theme.rs
+++ src/themes/from_zed_theme.rs
@@ -1,129 +1,13 @@
-use super::{
-    theme_descriptor::ThemeDescriptor, Color, DiagnosticStyles, HighlightName, Theme, UiStyles,
-};
+use super::{Color, DiagnosticStyles, HighlightName, Theme, UiStyles};
 use crate::{
     style::Style,
     themes::{GitGutterStyles, SyntaxStyles},
 };
 use itertools::Itertools;
 use my_proc_macros::hex;
-use shared::download::cache_download;
 use zed_theme::*;
 
-const ZED_THEME_AYU_URL: &str =
-    "https://raw.githubusercontent.com/zed-industries/zed/main/assets/themes/ayu/ayu.json";
-const ZED_THEME_ALABASTER_URL: &str =
-    "https://raw.githubusercontent.com/tsimoshka/zed-theme-alabaster/refs/heads/main/themes/alabaster-color-theme.json";
-const ZED_THEME_APATHY_URL: &str =
-    "https://raw.githubusercontent.com/mqual/themes/refs/heads/main/apathy_ki.json";
-const ZED_THEME_CATPPUCCIN_URL: &str =
-    "https://raw.githubusercontent.com/catppuccin/zed/main/themes/catppuccin-mauve.json";
-const ZED_THEME_DRACULA_URL: &str =
-    "https://raw.githubusercontent.com/dracula/zed/refs/heads/main/themes/dracula.json";
-const ZED_THEME_GITHUB_URL: &str = "https://raw.githubusercontent.com/PyaeSoneAungRgn/github-zed-theme/refs/heads/main/themes/github_theme.json";
-const ZED_THEME_GRUBER_DARKER_URL: &str =
-    "https://raw.githubusercontent.com/mqual/themes/refs/heads/main/gruber_darker_zed.json";
-const ZED_THEME_GRUVBOX_URL: &str =
-    "https://raw.githubusercontent.com/zed-industries/zed/main/assets/themes/gruvbox/gruvbox.json";
-const ZED_THEME_ONE_URL: &str =
-    "https://raw.githubusercontent.com/zed-industries/zed/main/assets/themes/one/one.json";
-const ZED_THEME_MODUS_URL: &str = "https://raw.githubusercontent.com/vitallium/zed-modus-themes/refs/heads/main/themes/modus.json";
-const ZED_THEME_MONOKAI_ST3_URL: &str =
-    "https://raw.githubusercontent.com/epmoyer/Zed-Monokai-Theme/main/monokai_st3.json";
-const ZED_THEME_MONOKAI_URL: &str =
-    "https://raw.githubusercontent.com/epmoyer/Zed-Monokai-Theme/main/monokai.json";
-const ZED_THEME_MQUAL_BLUE_URL: &str =
-    "https://raw.githubusercontent.com/mqual/themes/main/mqual_blue_zed.json";
-const ZED_THEME_NORD_URL: &str =
-    "https://raw.githubusercontent.com/mikasius/zed-nord-theme/refs/heads/master/themes/nord.json";
-const ZED_THEME_ROSE_PINE_URL: &str =
-    "https://raw.githubusercontent.com/rose-pine/zed/refs/heads/main/themes/rose-pine.json";
-const ZED_THEME_SOLARIZED_URL: &str = "https://raw.githubusercontent.com/harmtemolder/Solarized.zed/refs/heads/main/themes/solarized.json";
-const ZED_THEME_TOKYO_NIGHT_URL: &str = "https://raw.githubusercontent.com/ssaunderss/zed-tokyo-night/refs/heads/main/themes/tokyo-night.json";
-
-#[derive(serde::Deserialize)]
-struct ZedThemeManiftest {
-    themes: Vec<ThemeContent>,
-}
-
-/// Get all known Zed themes as a `ThemeDescriptor`.
-pub(crate) fn theme_descriptors() -> Vec<ThemeDescriptor> {
-    [
-        ("Ayu Dark", ZED_THEME_AYU_URL),
-        ("Ayu Light", ZED_THEME_AYU_URL),
-        ("Ayu Mirage", ZED_THEME_AYU_URL),
-        ("Alabaster", ZED_THEME_ALABASTER_URL),
-        ("Alabaster Dark", ZED_THEME_ALABASTER_URL),
-        ("Alabaster Mono", ZED_THEME_ALABASTER_URL),
-        ("Alabaster Dark Mono", ZED_THEME_ALABASTER_URL),
-        ("Alabaster BG", ZED_THEME_ALABASTER_URL),
-        ("Apathy Dark", ZED_THEME_APATHY_URL),
-        ("Apathy Light", ZED_THEME_APATHY_URL),
-        ("Catppuccin Frappé", ZED_THEME_CATPPUCCIN_URL),
-        ("Catppuccin Latte", ZED_THEME_CATPPUCCIN_URL),
-        ("Catppuccin Macchiato", ZED_THEME_CATPPUCCIN_URL),
-        ("Catppuccin Mocha", ZED_THEME_CATPPUCCIN_URL),
-        ("Dracula", ZED_THEME_DRACULA_URL),
-        ("Github Dark Colorblind", ZED_THEME_GITHUB_URL),
-        ("Github Dark Dimmed", ZED_THEME_GITHUB_URL),
-        ("Github Dark High Contrast", ZED_THEME_GITHUB_URL),
-        ("Github Dark Tritanopia", ZED_THEME_GITHUB_URL),
-        ("Github Dark", ZED_THEME_GITHUB_URL),
-        ("Github Light Colorblind", ZED_THEME_GITHUB_URL),
-        ("Github Light High Contrast", ZED_THEME_GITHUB_URL),
-        ("Github Light Tritanopia", ZED_THEME_GITHUB_URL),
-        ("Github Light", ZED_THEME_GITHUB_URL),
-        ("Gruber Darker", ZED_THEME_GRUBER_DARKER_URL),
-        ("Gruvbox Dark Hard", ZED_THEME_GRUVBOX_URL),
-        ("Gruvbox Dark Soft", ZED_THEME_GRUVBOX_URL),
-        ("Gruvbox Dark", ZED_THEME_GRUVBOX_URL),
-        ("Gruvbox Light Hard", ZED_THEME_GRUVBOX_URL),
-        ("Gruvbox Light Soft", ZED_THEME_GRUVBOX_URL),
-        ("Gruvbox Light", ZED_THEME_GRUVBOX_URL),
-        ("Modus Operandi Tinted", ZED_THEME_MODUS_URL),
-        ("Modus Operandi", ZED_THEME_MODUS_URL),
-        ("Modus Vivendi Tinted", ZED_THEME_MODUS_URL),
-        ("Modus Vivendi", ZED_THEME_MODUS_URL),
-        ("Monokai", ZED_THEME_MONOKAI_URL),
-        ("Monokai-ST3", ZED_THEME_MONOKAI_ST3_URL),
-        ("Mqual Blue", ZED_THEME_MQUAL_BLUE_URL),
-        ("Nord", ZED_THEME_NORD_URL),
-        ("Nord Light", ZED_THEME_NORD_URL),
-        ("One Dark", ZED_THEME_ONE_URL),
-        ("One Light", ZED_THEME_ONE_URL),
-        ("Rosé Pine Dawn", ZED_THEME_ROSE_PINE_URL),
-        ("Rosé Pine Moon", ZED_THEME_ROSE_PINE_URL),
-        ("Rosé Pine", ZED_THEME_ROSE_PINE_URL),
-        ("Solarized Dark", ZED_THEME_SOLARIZED_URL),
-        ("Solarized Light", ZED_THEME_SOLARIZED_URL),
-        ("Tokyo Night Light", ZED_THEME_TOKYO_NIGHT_URL),
-        ("Tokyo Night Storm", ZED_THEME_TOKYO_NIGHT_URL),
-        ("Tokyo Night", ZED_THEME_TOKYO_NIGHT_URL),
-    ]
-    .iter()
-    .map(|(name, url)| ThemeDescriptor::ZedTheme(name, url))
-    .collect()
-}
-
-pub(crate) fn from_url(name: &'static str, url: &'static str) -> anyhow::Result<Theme> {
-    let path = std::path::PathBuf::from(url);
-    let file_name = path
-        .file_name()
-        .ok_or_else(|| anyhow::anyhow!("The url ({:?}) should contain a file name.", url))?
-        .to_string_lossy();
-    let json_str = cache_download(url, "zed-themes", &file_name)?;
-    let manifest: ZedThemeManiftest = serde_json5::from_str(&json_str).map_err(|error| {
-        anyhow::anyhow!("Cannot parse JSON downloaded from {url:?} due to:\n{error:#?}")
-    })?;
-
-    manifest
-        .themes
-        .iter()
-        .find_map(|theme| (theme.name == name).then(|| from_theme_content(theme.clone())))
-        .ok_or_else(|| anyhow::anyhow!("could not find theme '{}'", name))
-}
-
-fn from_theme_content(theme: ThemeContent) -> Theme {
+pub(super) fn from_theme_content(theme: ThemeContent) -> Theme {
     let background = theme
         .style
         .editor_background
@@ -293,20 +177,5 @@ fn from_theme_content(theme: ThemeContent) -> Theme {
             super::HunkStyles::dark()
         },
         git_gutter: GitGutterStyles::new(),
-    }
-}
-
-#[cfg(test)]
-mod test_from_zed_theme {
-    use crate::themes::Theme;
-
-    #[test]
-    fn ensure_all_zed_themes_parse() -> anyhow::Result<()> {
-        // Expect no failure
-        let _: Vec<Theme> = super::theme_descriptors()
-            .iter()
-            .map(|theme| (*theme).to_theme())
-            .collect();
-        Ok(())
     }
 }
