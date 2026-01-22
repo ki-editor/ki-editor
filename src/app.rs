ki-editor/ki-editor

commit 31ca5f6e826852abd8e8ab7af37d92d2df104f27

refactor(keymap): delete meaning & use QWERTY everywhere (#1255)
We talk about keybinds in QWERTY all the time, and the extra indirection
hurts us on the whole. There is potentially some good followup work to
re-introduce some named constants, so we can make sure the same binds
are used in different layers (change = rename in space context).

src/app.rs
@@ -8,7 +8,7 @@ use crate::{
         editor::{
             Direction, DispatchEditor, Editor, IfCurrentNotFound, Movement, PriorChange, Reveal,
         },
-        editor_keymap::{KeyboardLayoutKind, Meaning},
+        editor_keymap::KeyboardLayoutKind,
         editor_keymap_printer::KeymapDisplayOption,
         file_explorer::FileExplorer,
         keymap_legend::{Keybinding, Keymap, KeymapLegendConfig, ReleaseKey},
@@ -404,6 +404,15 @@ impl<T: Frontend> App<T> {
                 });
             }
             event => {
+                // Translate key events so that other layouts pretend to be QWERTY for the rest of the code
+                let event = match event {
+                    Event::Key(key_event) => Event::Key(
+                        self.context
+                            .keyboard_layout_kind()
+                            .translate_key_event_to_qwerty(key_event),
+                    ),
+                    event => event,
+                };
                 let dispatches = component.borrow_mut().handle_event(&self.context, event);
                 self.handle_dispatches_result(dispatches)
                     .unwrap_or_else(|e| {
@@ -563,10 +572,11 @@ impl<T: Frontend> App<T> {
                             .last()
                             .map(|search| FlexLayoutComponent::Text(format!("{search:?}"))),
                         StatusLineComponent::Help => {
-                            let key = self
+                            let help_key = self
+                                .context
                                 .keyboard_layout_kind()
-                                .get_space_keymap_keybinding(&Meaning::SHelp);
-                            Some(FlexLayoutComponent::Text(format!("Help(Space+{key})")))
+                                .translate_char_to_qwerty('/');
+                            Some(FlexLayoutComponent::Text(format!("Help(Space+{help_key})")))
                         }
                         StatusLineComponent::KeyboardLayout => Some(FlexLayoutComponent::Text(
                             self.keyboard_layout_kind().display().to_string(),
@@ -1089,7 +1099,7 @@ impl<T: Frontend> App<T> {
                 self.add_quickfix_list_entries(locations)?
             }
             Dispatch::AppliedEdits { path, edits } => self.handle_applied_edits(path, edits),
-            Dispatch::ExecuteLeaderMeaning(meaning) => self.execute_leader_meaning(meaning)?,
+            Dispatch::ExecuteLeaderKey(key) => self.execute_leader_key(key)?,
             Dispatch::ShowBufferSaveConflictPrompt {
                 path,
                 content_editor,
@@ -1886,18 +1896,8 @@ impl<T: Frontend> App<T> {
         self.handle_dispatch(Dispatch::ShowKeymapLegend(KeymapLegendConfig {
             title: prompt.title.to_string(),
             keymap: Keymap::new(&[
-                Keybinding::new(
-                    self.keyboard_layout_kind()
-                        .get_yes_no_keymap_keybinding(&Meaning::Yes__),
-                    "Yes".to_string(),
-                    *prompt.yes,
-                ),
-                Keybinding::new(
-                    self.keyboard_layout_kind()
-                        .get_yes_no_keymap_keybinding(&Meaning::No___),
-                    "No".to_string(),
-                    Dispatch::Null,
-                ),
+                Keybinding::new("d", "Yes".to_string(), *prompt.yes),
+                Keybinding::new("k", "No".to_string(), Dispatch::Null),
             ]),
         }))
     }
@@ -3247,8 +3247,8 @@ Conflict markers will be injected in areas that cannot be merged gracefully."
         ))
     }
 
-    fn execute_leader_meaning(&mut self, meaning: Meaning) -> anyhow::Result<()> {
-        if let Some((_, _, script)) = custom_keymap().into_iter().find(|(m, _, _)| *m == meaning) {
+    fn execute_leader_key(&mut self, key: String) -> anyhow::Result<()> {
+        if let Some((_, _, script)) = custom_keymap().into_iter().find(|(k, _, _)| k == &key) {
             let output = {
                 let component = self.current_component();
                 let borrow = component.borrow();
@@ -3586,7 +3586,7 @@ pub enum Dispatch {
         edits: Vec<Edit>,
         path: CanonicalizedPath,
     },
-    ExecuteLeaderMeaning(Meaning),
+    ExecuteLeaderKey(String),
     ShowBufferSaveConflictPrompt {
         path: CanonicalizedPath,
         content_filesystem: String,

src/components/editor_keymap_printer.rs
@@ -253,34 +253,33 @@ impl KeymapPrintSections {
         let sections: Vec<KeymapPrintSection> = [
             KeymapPrintSection::from_keymap(
                 "Insert".to_string(),
-                &editor.insert_mode_keymap(false, &context),
+                &editor.insert_mode_keymap(false),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Normal".to_string(),
-                &Keymap::new(&editor.normal_mode_keymap(&context, Default::default(), None)),
+                &Keymap::new(&editor.normal_mode_keymap(Default::default(), None)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Movements".to_string(),
-                &Keymap::new(&editor.keymap_core_movements(&context, None)),
+                &Keymap::new(&editor.keymap_core_movements(None)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Primary Selection Modes".to_string(),
-                &Keymap::new(&editor.keymap_primary_selection_modes(&context, None)),
+                &Keymap::new(&editor.keymap_primary_selection_modes(None)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Secondary Selection Modes Init".to_string(),
-                &Keymap::new(&editor.keymap_secondary_selection_modes_init(&context, None)),
+                &Keymap::new(&editor.keymap_secondary_selection_modes_init(None)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Secondary Selection Modes (Local)".to_string(),
                 &editor
                     .secondary_selection_modes_keymap_legend_config(
-                        &context,
                         Scope::Local,
                         IfCurrentNotFound::LookForward,
                         None,
@@ -292,7 +291,6 @@ impl KeymapPrintSections {
                 "Secondary Selection Modes (Global)".to_string(),
                 &editor
                     .secondary_selection_modes_keymap_legend_config(
-                        &context,
                         Scope::Global,
                         IfCurrentNotFound::LookForward,
                         None,
@@ -302,12 +300,12 @@ impl KeymapPrintSections {
             ),
             KeymapPrintSection::from_keymap(
                 "Actions".to_string(),
-                &Keymap::new(&editor.keymap_actions(&Default::default(), false, &context, None)),
+                &Keymap::new(&editor.keymap_actions(&Default::default(), false, None)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Other Movements".to_string(),
-                &Keymap::new(&editor.keymap_other_movements(&context)),
+                &Keymap::new(&editor.keymap_other_movements()),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
@@ -317,35 +315,29 @@ impl KeymapPrintSections {
             ),
             KeymapPrintSection::from_keymap(
                 "Space Context".to_string(),
-                &editor.space_context_keymap_legend_config(&context).keymap(),
+                &editor.space_context_keymap_legend_config().keymap(),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space Editor".to_string(),
-                &editor.space_editor_keymap_legend_config(&context).keymap(),
+                &editor.space_editor_keymap_legend_config().keymap(),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space Pick".to_string(),
-                &editor.space_pick_keymap_legend_config(&context).keymap(),
+                &editor.space_pick_keymap_legend_config().keymap(),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "File Explorer Actions".to_string(),
-                &Keymap::new(&editor.keymap_overridable(
-                    &file_explorer_normal_mode_override(),
-                    true,
-                    &context,
-                )),
+                &Keymap::new(
+                    &editor.keymap_overridable(&file_explorer_normal_mode_override(), true),
+                ),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Extend".to_string(),
-                &Keymap::new(&editor.keymap_overridable(
-                    &extend_mode_normal_mode_override(&context),
-                    true,
-                    &context,
-                )),
+                &Keymap::new(&editor.keymap_overridable(&extend_mode_normal_mode_override(), true)),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
@@ -353,34 +345,33 @@ impl KeymapPrintSections {
                 &Keymap::new(&editor.keymap_overridable(
                     &multicursor_mode_normal_mode_override(Direction::End),
                     true,
-                    &context,
                 )),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Completion Items".to_string(),
-                &completion_item_keymap(&context),
+                &completion_item_keymap(),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Universal Keymap".to_string(),
-                &Keymap::new(&editor.keymap_universal(&context)),
+                &Keymap::new(&editor.keymap_universal()),
                 layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Transform".to_string(),
-                &Keymap::new(&editor.keymap_transform(&context)),
+                &Keymap::new(&editor.keymap_transform()),
                 layout,
             ),
-            KeymapPrintSection::from_keymap("Paste".to_string(), &paste_keymap(&context), layout),
+            KeymapPrintSection::from_keymap("Paste".to_string(), &paste_keymap(), layout),
             KeymapPrintSection::from_keymap(
                 "Multi-cursor".to_string(),
-                &multicursor_keymap(&context),
+                &multicursor_keymap(),
                 layout,
             ),
-            KeymapPrintSection::from_keymap("Cut".to_string(), &cut_keymap(&context), layout),
-            KeymapPrintSection::from_keymap("Swap".to_string(), &swap_keymap(&context), layout),
-            KeymapPrintSection::from_keymap("Delete".to_string(), &delete_keymap(&context), layout),
+            KeymapPrintSection::from_keymap("Cut".to_string(), &cut_keymap(), layout),
+            KeymapPrintSection::from_keymap("Swap".to_string(), &swap_keymap(), layout),
+            KeymapPrintSection::from_keymap("Delete".to_string(), &delete_keymap(), layout),
         ]
         .to_vec();
 

src/components/keymap_legend.rs
@@ -5,7 +5,7 @@ use my_proc_macros::key;
 
 use crate::{
     app::{Dispatch, Dispatches},
-    components::{editor_keymap::Meaning, editor_keymap_printer::KeymapDisplayOption},
+    components::editor_keymap_printer::KeymapDisplayOption,
     context::Context,
     rectangle::Rectangle,
 };
@@ -31,22 +31,22 @@ pub struct KeymapLegendConfig {
 }
 
 pub struct MomentaryLayer {
-    pub meaning: Meaning,
+    pub key: &'static str,
     pub description: String,
     pub config: KeymapLegendConfig,
     pub on_tap: Option<OnTap>,
 }
 
 struct ParsedReleaseKey {
+    key: &'static str,
     key_event: KeyEvent,
-    meaning: Meaning,
     on_tap: Option<OnTap>,
     other_keys_pressed: bool,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ReleaseKey {
-    meaning: Meaning,
+    key: &'static str,
     on_tap: Option<OnTap>,
     /// Initial value should be false.
     /// This field is necessary for implementing `on_tap`.
@@ -69,9 +69,9 @@ impl OnTap {
 }
 
 impl ReleaseKey {
-    pub fn new(meaning: Meaning, on_tap: Option<OnTap>) -> Self {
+    pub fn new(key: &'static str, on_tap: Option<OnTap>) -> Self {
         Self {
-            meaning,
+            key,
             on_tap,
             other_keys_pressed: false,
         }
@@ -162,24 +162,20 @@ impl Keybinding {
     }
 
     pub fn momentary_layer(
-        context: &Context,
         MomentaryLayer {
-            meaning,
+            key,
             description,
             config,
             on_tap,
         }: MomentaryLayer,
     ) -> Keybinding {
-        let key = context
-            .keyboard_layout_kind()
-            .get_normal_keymap_keybinding(&meaning);
         Keybinding {
             key,
             short_description: None,
             description,
             dispatch: Dispatch::ShowKeymapLegendWithReleaseKey(
                 config,
-                ReleaseKey::new(meaning, on_tap),
+                ReleaseKey::new(key, on_tap),
             ),
             event: parse_key_event(key).unwrap(),
         }
@@ -274,16 +270,11 @@ impl KeymapLegend {
             .unwrap_or_default();
 
         let release_key = release_key.map(|release_key| ParsedReleaseKey {
+            key: release_key.key,
             key_event: KeyEvent {
                 kind: crossterm::event::KeyEventKind::Release,
-                ..parse_key_event(
-                    context
-                        .keyboard_layout_kind()
-                        .get_normal_keymap_keybinding(&release_key.meaning),
-                )
-                .unwrap()
+                ..parse_key_event(release_key.key).unwrap()
             },
-            meaning: release_key.meaning,
             on_tap: release_key.on_tap,
             other_keys_pressed: release_key.other_keys_pressed,
         });
@@ -364,7 +355,7 @@ impl Component for KeymapLegend {
                                     Dispatch::ShowKeymapLegendWithReleaseKey(
                                         self.config.clone(),
                                         ReleaseKey {
-                                            meaning: release_key.meaning.clone(),
+                                            key: release_key.key,
                                             on_tap: release_key.on_tap.clone(),
                                             other_keys_pressed: true,
                                         },
@@ -600,7 +591,7 @@ mod test_keymap_legend {
             },
             &Context::default(),
             Some(ReleaseKey::new(
-                Meaning::AgSlL,
+                "Y",
                 Some(OnTap::new("Conichihuahua", Dispatches::default())),
             )),
         );
@@ -637,7 +628,6 @@ Release hold: Conichihuahua
     /// When release key is defined and the release key is immediately received
     /// before any actions in the keymap is executed, the on tap dispatches should be fired.
     fn release_key_on_tap() -> anyhow::Result<()> {
-        let context = Context::default();
         execute_test(|s| {
             Box::new([
                 App(OpenFile {
@@ -652,7 +642,7 @@ Release hold: Conichihuahua
                         keymap: Keymap::new(&[]),
                     },
                     ReleaseKey::new(
-                        Meaning::Paste,
+                        "b",
                         Some(OnTap::new(
                             "",
                             Dispatches::one(Dispatch::ToEditor(SetContent(
@@ -665,13 +655,9 @@ Release hold: Conichihuahua
                 Expect(AppGridContains("LEGEND_TITLE")),
                 // Simulate key release
                 App(HandleKeyEvent(
-                    parse_key_event(
-                        context
-                            .keyboard_layout_kind()
-                            .get_normal_keymap_keybinding(&Meaning::Paste),
-                    )
-                    .unwrap()
-                    .set_event_kind(KeyEventKind::Release),
+                    parse_key_event("b")
+                        .unwrap()
+                        .set_event_kind(KeyEventKind::Release),
                 )),
                 Expect(CurrentComponentContent("on tapped!")),
             ])
@@ -681,7 +667,6 @@ Release hold: Conichihuahua
     #[test]
     fn when_release_key_is_defined_legend_should_show_until_release_key_is_received(
     ) -> anyhow::Result<()> {
-        let context = Context::default();
         execute_test(|s| {
             Box::new([
                 App(OpenFile {
@@ -699,7 +684,7 @@ Release hold: Conichihuahua
                             Dispatch::ToEditor(Insert("hello".to_string())),
                         )]),
                     },
-                    ReleaseKey::new(Meaning::Paste, None),
+                    ReleaseKey::new("b", None),
                 )),
                 // Expect the keymap legend is opened
                 Expect(AppGridContains("LEGEND_TITLE")),
@@ -709,13 +694,9 @@ Release hold: Conichihuahua
                 Expect(AppGridContains("LEGEND_TITLE")),
                 // Simulate key release
                 App(HandleKeyEvent(
-                    parse_key_event(
-                        context
-                            .keyboard_layout_kind()
-                            .get_normal_keymap_keybinding(&Meaning::Paste),
-                    )
-                    .unwrap()
-                    .set_event_kind(KeyEventKind::Release),
+                    parse_key_event("b")
+                        .unwrap()
+                        .set_event_kind(KeyEventKind::Release),
                 )),
                 // Expect the legend is closed
                 Expect(Not(Box::new(AppGridContains("LEGEND_TITLE")))),

event/src/event.rs
@@ -168,7 +168,7 @@ pub enum KeyModifiers {
 }
 
 impl KeyModifiers {
-    pub(crate) fn add_shift(self, shift: bool) -> KeyModifiers {
+    pub(crate) fn add_shift(self, shift: bool) -> Self {
         use KeyModifiers::*;
         if !shift {
             return self;
@@ -180,6 +180,28 @@ impl KeyModifiers {
             CtrlAlt => CtrlAltShift,
             Unknown => Shift,
             _ => self,
+        }
+    }
+
+    pub(crate) fn remove_shift(self, shift: bool) -> Self {
+        use KeyModifiers::*;
+        if shift {
+            return self;
+        }
+        match self {
+            Shift => None,
+            CtrlShift => Ctrl,
+            AltShift => Alt,
+            CtrlAltShift => CtrlAlt,
+            _ => self,
+        }
+    }
+
+    pub fn set_shift(self, shift: bool) -> Self {
+        if shift {
+            self.add_shift(shift)
+        } else {
+            self.remove_shift(shift)
         }
     }
 

ki-vscode/src/managers/keyboard_manager.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+ll;
 import * as zlib from "node:zlib";
 import type { Dispatcher } from "../dispatcher";
 import type { Logger } from "../logger";

commit 6f6e1a5a32d711d6351bdf1c664e9ec0ed326d68

fix(keymap): fix keymap legend printing
Bug introduced by #1255.

src/app.rs
@@ -1800,7 +1800,6 @@ impl<T: Frontend> App<T> {
         if self.is_running_as_embedded() {
             let title = keymap_legend_config.title.clone();
             let body = keymap_legend_config.display(
-                self.context.keyboard_layout_kind(),
                 usize::MAX,
                 &KeymapDisplayOption {
                     show_alt: false,

src/components/editor_keymap_printer.rs
@@ -10,7 +10,7 @@ use crate::{
     app::Scope,
     components::{
         editor::Editor,
-        editor_keymap::{shifted, KeyboardLayout},
+        editor_keymap::{shifted, QWERTY},
         editor_keymap_legend::{cut_keymap, delete_keymap, paste_keymap, swap_keymap},
     },
     context::Context,
@@ -79,10 +79,10 @@ impl Key {
 }
 
 impl KeymapPrintSection {
-    pub fn from_keymap(name: String, keymap: &Keymap, keyboard_layout: &KeyboardLayout) -> Self {
+    pub fn from_keymap(name: String, keymap: &Keymap) -> Self {
         KeymapPrintSection {
             name,
-            keys: keyboard_layout
+            keys: QWERTY
                 .iter()
                 .map(|row| {
                     row.iter()
@@ -244,33 +244,27 @@ pub struct KeymapPrintSections {
 impl KeymapPrintSections {
     pub fn new() -> Self {
         let context = Context::new(CanonicalizedPath::try_from(".").unwrap(), false, None);
-        let layout = context.keyboard_layout_kind().get_keyboard_layout();
         let editor = Editor::from_text(Option::None, "");
         let sections: Vec<KeymapPrintSection> = [
             KeymapPrintSection::from_keymap(
                 "Insert".to_string(),
                 &editor.insert_mode_keymap(false),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Normal".to_string(),
                 &Keymap::new(&editor.normal_mode_keymap(Default::default(), None)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Movements".to_string(),
                 &Keymap::new(&editor.keymap_core_movements(None)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Primary Selection Modes".to_string(),
                 &Keymap::new(&editor.keymap_primary_selection_modes(None)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Secondary Selection Modes Init".to_string(),
                 &Keymap::new(&editor.keymap_secondary_selection_modes_init(None)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Secondary Selection Modes (Local)".to_string(),
@@ -281,7 +275,6 @@ impl KeymapPrintSections {
                         None,
                     )
                     .keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Secondary Selection Modes (Global)".to_string(),
@@ -292,79 +285,65 @@ impl KeymapPrintSections {
                         None,
                     )
                     .keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Actions".to_string(),
                 &Keymap::new(&editor.keymap_actions(&Default::default(), false, None)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Other Movements".to_string(),
                 &Keymap::new(&editor.keymap_other_movements()),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space".to_string(),
                 &editor.space_keymap_legend_config(&context).keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space Context".to_string(),
                 &editor.space_context_keymap_legend_config().keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space Editor".to_string(),
                 &editor.space_editor_keymap_legend_config().keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Space Pick".to_string(),
                 &editor.space_pick_keymap_legend_config().keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "File Explorer Actions".to_string(),
                 &Keymap::new(
                     &editor.keymap_overridable(&file_explorer_normal_mode_override(), true),
                 ),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Extend".to_string(),
                 &Keymap::new(&editor.keymap_overridable(&extend_mode_normal_mode_override(), true)),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Completion Items".to_string(),
                 &completion_item_keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Universal Keymap".to_string(),
                 &Keymap::new(&editor.keymap_universal()),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Transform".to_string(),
                 &Keymap::new(&editor.keymap_transform()),
-                layout,
             ),
-            KeymapPrintSection::from_keymap("Paste".to_string(), &paste_keymap(), layout),
+            KeymapPrintSection::from_keymap("Paste".to_string(), &paste_keymap()),
             KeymapPrintSection::from_keymap(
                 "Multi-cursor Momentary Layer".to_string(),
                 &editor.multicursor_momentary_layer_keymap(),
-                layout,
             ),
             KeymapPrintSection::from_keymap(
                 "Multi-cursor Menu".to_string(),
                 &editor.multicursor_menu_keymap(),
-                layout,
             ),
-            KeymapPrintSection::from_keymap("Cut".to_string(), &cut_keymap(), layout),
-            KeymapPrintSection::from_keymap("Swap".to_string(), &swap_keymap(), layout),
-            KeymapPrintSection::from_keymap("Delete".to_string(), &delete_keymap(), layout),
+            KeymapPrintSection::from_keymap("Cut".to_string(), &cut_keymap()),
+            KeymapPrintSection::from_keymap("Swap".to_string(), &swap_keymap()),
+            KeymapPrintSection::from_keymap("Delete".to_string(), &delete_keymap()),
         ]
         .to_vec();
 

src/components/keymap_legend.rs
@@ -13,14 +13,12 @@ use crate::{
 use super::{
     component::Component,
     editor::{Direction, Editor, Mode},
-    editor_keymap::KeyboardLayoutKind,
     editor_keymap_printer::KeymapPrintSection,
 };
 
 pub struct KeymapLegend {
     editor: Editor,
     config: KeymapLegendConfig,
-    keymap_layout_kind: super::editor_keymap::KeyboardLayoutKind,
     release_key: Option<ParsedReleaseKey>,
 }
 
@@ -103,18 +101,8 @@ impl ReleaseKey {
 #[derive(Debug, Clone, PartialEq)]
 pub struct Keymap(Vec<Keybinding>);
 impl Keymap {
-    fn display(
-        &self,
-        keyboard_layout_kind: &KeyboardLayoutKind,
-        terminal_width: usize,
-        option: &KeymapDisplayOption,
-    ) -> String {
-        KeymapPrintSection::from_keymap(
-            "".to_string(),
-            self,
-            keyboard_layout_kind.get_keyboard_layout(),
-        )
-        .display(terminal_width, option)
+    fn display(&self, terminal_width: usize, option: &KeymapDisplayOption) -> String {
+        KeymapPrintSection::from_keymap("".to_string(), self).display(terminal_width, option)
     }
     pub fn new(keybindings: &[Keybinding]) -> Self {
         Self(keybindings.to_vec())
@@ -134,13 +122,8 @@ impl Keymap {
 }
 
 impl KeymapLegendConfig {
-    pub fn display(
-        &self,
-        keyboard_layout_kind: &KeyboardLayoutKind,
-        width: usize,
-        option: &KeymapDisplayOption,
-    ) -> String {
-        self.keymap.display(keyboard_layout_kind, width, option)
+    pub fn display(&self, width: usize, option: &KeymapDisplayOption) -> String {
+        self.keymap.display(width, option)
     }
 
     pub fn keymap(&self) -> Keymap {
@@ -306,7 +289,6 @@ impl KeymapLegend {
         KeymapLegend {
             editor,
             config,
-            keymap_layout_kind: *context.keyboard_layout_kind(),
             release_key,
         }
     }
@@ -320,7 +302,6 @@ impl KeymapLegend {
 
     fn display(&self) -> String {
         let content = self.config.display(
-            &self.keymap_layout_kind,
             self.editor.rectangle().width,
             &KeymapDisplayOption {
                 show_alt: true,
@@ -478,10 +459,8 @@ mod test_keymap_legend {
             ]
             .to_vec(),
         );
-        let context = Context::default();
         let actual = keymap
             .display(
-                context.keyboard_layout_kind(),
                 100,
                 &KeymapDisplayOption {
                     show_alt: false,
@@ -504,7 +483,6 @@ mod test_keymap_legend {
 
         let actual = keymap
             .display(
-                context.keyboard_layout_kind(),
                 100,
                 &KeymapDisplayOption {
                     show_alt: true,
@@ -542,10 +520,8 @@ mod test_keymap_legend {
             ]
             .to_vec(),
         );
-        let context = Context::default();
         let actual = keymap
             .display(
-                context.keyboard_layout_kind(),
                 50,
                 &KeymapDisplayOption {
                     show_alt: true,
@@ -589,10 +565,8 @@ mod test_keymap_legend {
             ]
             .to_vec(),
         );
-        let context = Context::default();
         let actual = keymap
             .display(
-                context.keyboard_layout_kind(),
                 10,
                 &KeymapDisplayOption {
                     show_alt: true,