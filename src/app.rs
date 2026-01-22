ki-editor/ki-editor

commit 31ca5f6e826852abd8e8ab7af37d92d2df104f27

refactor(keymap): delete meaning & use QWERTY everywhere (#1255)
We talk about keybinds in QWERTY all the time, and the extra indirection
hurts us on the whole. There is potentially some good followup work to
re-introduce some named constants, so we can make sure the same binds
are used in different layers (change = rename in space context).

src/app.rs
@@ -8,7 +8,7 @@ use crate::{
         editor::{
             Direction, DispatchEditor, Editor, IfCurrentNotFound, Movement, PriorChange, Reveal,
         },
-        editor_keymap::{KeyboardLayoutKind, Meaning},
+        editor_keymap::KeyboardLayoutKind,
         editor_keymap_printer::KeymapDisplayOption,
         file_explorer::FileExplorer,
         keymap_legend::{Keybinding, Keymap, KeymapLegendConfig, ReleaseKey},
@@ -404,6 +404,15 @@ impl<T: Frontend> App<T> {
                 });
             }
             event => {
+                // Translate key events so that other layouts pretend to be QWERTY for the rest of the code
+                let event = match event {
+                    Event::Key(key_event) => Event::Key(
+                        self.context
+                            .keyboard_layout_kind()
+                            .translate_key_event_to_qwerty(key_event),
+                    ),
+                    event => event,
+                };
                 let dispatches = component.borrow_mut().handle_event(&self.context, event);
                 self.handle_dispatches_result(dispatches)
                     .unwrap_or_else(|e| {
@@ -563,10 +572,11 @@ impl<T: Frontend> App<T> {
                             .last()
                             .map(|search| FlexLayoutComponent::Text(format!("{search:?}"))),
                         StatusLineComponent::Help => {
-                            let key = self
+                            let help_key = self
+                                .context
                                 .keyboard_layout_kind()
-                                .get_space_keymap_keybinding(&Meaning::SHelp);
-                            Some(FlexLayoutComponent::Text(format!("Help(Space+{key})")))
+                                .translate_char_to_qwerty('/');
+                            Some(FlexLayoutComponent::Text(format!("Help(Space+{help_key})")))
                         }
                         StatusLineComponent::KeyboardLayout => Some(FlexLayoutComponent::Text(
                             self.keyboard_layout_kind().display().to_string(),
@@ -1089,7 +1099,7 @@ impl<T: Frontend> App<T> {
                 self.add_quickfix_list_entries(locations)?
             }
             Dispatch::AppliedEdits { path, edits } => self.handle_applied_edits(path, edits),
-            Dispatch::ExecuteLeaderMeaning(meaning) => self.execute_leader_meaning(meaning)?,
+            Dispatch::ExecuteLeaderKey(key) => self.execute_leader_key(key)?,
             Dispatch::ShowBufferSaveConflictPrompt {
                 path,
                 content_editor,
@@ -1886,18 +1896,8 @@ impl<T: Frontend> App<T> {
         self.handle_dispatch(Dispatch::ShowKeymapLegend(KeymapLegendConfig {
             title: prompt.title.to_string(),
             keymap: Keymap::new(&[
-                Keybinding::new(
-                    self.keyboard_layout_kind()
-                        .get_yes_no_keymap_keybinding(&Meaning::Yes__),
-                    "Yes".to_string(),
-                    *prompt.yes,
-                ),
-                Keybinding::new(
-                    self.keyboard_layout_kind()
-                        .get_yes_no_keymap_keybinding(&Meaning::No___),
-                    "No".to_string(),
-                    Dispatch::Null,
-                ),
+                Keybinding::new("d", "Yes".to_string(), *prompt.yes),
+                Keybinding::new("k", "No".to_string(), Dispatch::Null),
             ]),
         }))
     }
@@ -3247,8 +3247,8 @@ Conflict markers will be injected in areas that cannot be merged gracefully."
         ))
     }
 
-    fn execute_leader_meaning(&mut self, meaning: Meaning) -> anyhow::Result<()> {
-        if let Some((_, _, script)) = custom_keymap().into_iter().find(|(m, _, _)| *m == meaning) {
+    fn execute_leader_key(&mut self, key: String) -> anyhow::Result<()> {
+        if let Some((_, _, script)) = custom_keymap().into_iter().find(|(k, _, _)| k == &key) {
             let output = {
                 let component = self.current_component();
                 let borrow = component.borrow();
@@ -3586,7 +3586,7 @@ pub enum Dispatch {
         edits: Vec<Edit>,
         path: CanonicalizedPath,
     },
-    ExecuteLeaderMeaning(Meaning),
+    ExecuteLeaderKey(String),
     ShowBufferSaveConflictPrompt {
         path: CanonicalizedPath,
         content_filesystem: String,

src/components/editor.rs
@@ -355,7 +355,7 @@ impl Component for Editor {
             EnterNewline => return self.enter_newline(context),
             DeleteCurrentCursor(direction) => self.delete_current_cursor(direction),
             BreakSelection => return self.break_selection(context),
-            ShowHelp => return self.show_help(context),
+            ShowHelp => return self.show_help(),
             HandleEsc => {
                 self.disable_selection_extension();
                 self.mode = Mode::Normal;
@@ -1604,7 +1604,7 @@ impl Editor {
         context: &Context,
         key_event: KeyEvent,
     ) -> anyhow::Result<Dispatches> {
-        match self.handle_universal_key(key_event, context)? {
+        match self.handle_universal_key(key_event)? {
             HandleEventResult::Ignored(key_event) => {
                 if let Some(dispatches) = self.handle_jump_mode(context, &key_event) {
                     dispatches
@@ -1617,7 +1617,7 @@ impl Editor {
                         context,
                     )
                 } else {
-                    let keymap_legend_config = self.get_current_keymap_legend_config(context);
+                    let keymap_legend_config = self.get_current_keymap_legend_config();
 
                     if let Some(keymap) = keymap_legend_config.keymap().get(&key_event) {
                         return Ok(keymap.get_dispatches());
@@ -3849,9 +3849,8 @@ impl Editor {
     pub fn insert_mode_keymap(
         &self,
         include_universal_keymap: bool,
-        context: &Context,
     ) -> super::keymap_legend::Keymap {
-        self.insert_mode_keymap_legend_config(include_universal_keymap, context)
+        self.insert_mode_keymap_legend_config(include_universal_keymap)
             .keymap()
     }
 
@@ -3859,19 +3858,16 @@ impl Editor {
         self.normal_mode_override = Some(normal_mode_override)
     }
 
-    fn show_help(&self, context: &Context) -> Result<Dispatches, anyhow::Error> {
+    fn show_help(&self) -> Result<Dispatches, anyhow::Error> {
         Ok(Dispatches::one(Dispatch::ShowKeymapLegend(
-            self.get_current_keymap_legend_config(context),
+            self.get_current_keymap_legend_config(),
         )))
     }
 
-    fn get_current_keymap_legend_config(
-        &self,
-        context: &Context,
-    ) -> super::keymap_legend::KeymapLegendConfig {
+    fn get_current_keymap_legend_config(&self) -> super::keymap_legend::KeymapLegendConfig {
         match self.mode {
-            Mode::Insert => self.insert_mode_keymap_legend_config(true, context),
-            _ => self.normal_mode_keymap_legend_config(context, None, None),
+            Mode::Insert => self.insert_mode_keymap_legend_config(true),
+            _ => self.normal_mode_keymap_legend_config(None, None),
         }
     }
 

src/components/keymap_legend.rs
@@ -5,7 +5,7 @@ use my_proc_macros::key;
 
 use crate::{
     app::{Dispatch, Dispatches},
-    components::{editor_keymap::Meaning, editor_keymap_printer::KeymapDisplayOption},
+    components::editor_keymap_printer::KeymapDisplayOption,
     context::Context,
     rectangle::Rectangle,
 };
@@ -31,22 +31,22 @@ pub struct KeymapLegendConfig {
 }
 
 pub struct MomentaryLayer {
-    pub meaning: Meaning,
+    pub key: &'static str,
     pub description: String,
     pub config: KeymapLegendConfig,
     pub on_tap: Option<OnTap>,
 }
 
 struct ParsedReleaseKey {
+    key: &'static str,
     key_event: KeyEvent,
-    meaning: Meaning,
     on_tap: Option<OnTap>,
     other_keys_pressed: bool,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ReleaseKey {
-    meaning: Meaning,
+    key: &'static str,
     on_tap: Option<OnTap>,
     /// Initial value should be false.
     /// This field is necessary for implementing `on_tap`.
@@ -69,9 +69,9 @@ impl OnTap {
 }
 
 impl ReleaseKey {
-    pub fn new(meaning: Meaning, on_tap: Option<OnTap>) -> Self {
+    pub fn new(key: &'static str, on_tap: Option<OnTap>) -> Self {
         Self {
-            meaning,
+            key,
             on_tap,
             other_keys_pressed: false,
         }
@@ -162,24 +162,20 @@ impl Keybinding {
     }
 
     pub fn momentary_layer(
-        context: &Context,
         MomentaryLayer {
-            meaning,
+            key,
             description,
             config,
             on_tap,
         }: MomentaryLayer,
     ) -> Keybinding {
-        let key = context
-            .keyboard_layout_kind()
-            .get_normal_keymap_keybinding(&meaning);
         Keybinding {
             key,
             short_description: None,
             description,
             dispatch: Dispatch::ShowKeymapLegendWithReleaseKey(
                 config,
-                ReleaseKey::new(meaning, on_tap),
+                ReleaseKey::new(key, on_tap),
             ),
             event: parse_key_event(key).unwrap(),
         }
@@ -274,16 +270,11 @@ impl KeymapLegend {
             .unwrap_or_default();
 
         let release_key = release_key.map(|release_key| ParsedReleaseKey {
+            key: release_key.key,
             key_event: KeyEvent {
                 kind: crossterm::event::KeyEventKind::Release,
-                ..parse_key_event(
-                    context
-                        .keyboard_layout_kind()
-                        .get_normal_keymap_keybinding(&release_key.meaning),
-                )
-                .unwrap()
+                ..parse_key_event(release_key.key).unwrap()
             },
-            meaning: release_key.meaning,
             on_tap: release_key.on_tap,
             other_keys_pressed: release_key.other_keys_pressed,
         });
@@ -364,7 +355,7 @@ impl Component for KeymapLegend {
                                     Dispatch::ShowKeymapLegendWithReleaseKey(
                                         self.config.clone(),
                                         ReleaseKey {
-                                            meaning: release_key.meaning.clone(),
+                                            key: release_key.key,
                                             on_tap: release_key.on_tap.clone(),
                                             other_keys_pressed: true,
                                         },
@@ -600,7 +591,7 @@ mod test_keymap_legend {
             },
             &Context::default(),
             Some(ReleaseKey::new(
-                Meaning::AgSlL,
+                "Y",
                 Some(OnTap::new("Conichihuahua", Dispatches::default())),
             )),
         );
@@ -637,7 +628,6 @@ Release hold: Conichihuahua
     /// When release key is defined and the release key is immediately received
     /// before any actions in the keymap is executed, the on tap dispatches should be fired.
     fn release_key_on_tap() -> anyhow::Result<()> {
-        let context = Context::default();
         execute_test(|s| {
             Box::new([
                 App(OpenFile {
@@ -652,7 +642,7 @@ Release hold: Conichihuahua
                         keymap: Keymap::new(&[]),
                     },
                     ReleaseKey::new(
-                        Meaning::Paste,
+                        "b",
                         Some(OnTap::new(
                             "",
                             Dispatches::one(Dispatch::ToEditor(SetContent(
@@ -665,13 +655,9 @@ Release hold: Conichihuahua
                 Expect(AppGridContains("LEGEND_TITLE")),
                 // Simulate key release
                 App(HandleKeyEvent(
-                    parse_key_event(
-                        context
-                            .keyboard_layout_kind()
-                            .get_normal_keymap_keybinding(&Meaning::Paste),
-                    )
-                    .unwrap()
-                    .set_event_kind(KeyEventKind::Release),
+                    parse_key_event("b")
+                        .unwrap()
+                        .set_event_kind(KeyEventKind::Release),
                 )),
                 Expect(CurrentComponentContent("on tapped!")),
             ])
@@ -681,7 +667,6 @@ Release hold: Conichihuahua
     #[test]
     fn when_release_key_is_defined_legend_should_show_until_release_key_is_received(
     ) -> anyhow::Result<()> {
-        let context = Context::default();
         execute_test(|s| {
             Box::new([
                 App(OpenFile {
@@ -699,7 +684,7 @@ Release hold: Conichihuahua
                             Dispatch::ToEditor(Insert("hello".to_string())),
                         )]),
                     },
-                    ReleaseKey::new(Meaning::Paste, None),
+                    ReleaseKey::new("b", None),
                 )),
                 // Expect the keymap legend is opened
                 Expect(AppGridContains("LEGEND_TITLE")),
@@ -709,13 +694,9 @@ Release hold: Conichihuahua
                 Expect(AppGridContains("LEGEND_TITLE")),
                 // Simulate key release
                 App(HandleKeyEvent(
-                    parse_key_event(
-                        context
-                            .keyboard_layout_kind()
-                            .get_normal_keymap_keybinding(&Meaning::Paste),
-                    )
-                    .unwrap()
-                    .set_event_kind(KeyEventKind::Release),
+                    parse_key_event("b")
+                        .unwrap()
+                        .set_event_kind(KeyEventKind::Release),
                 )),
                 // Expect the legend is closed
                 Expect(Not(Box::new(AppGridContains("LEGEND_TITLE")))),

src/components/suggestive_editor.rs
@@ -16,7 +16,7 @@ use std::{cell::RefCell, rc::Rc};
 
 use super::dropdown::{Dropdown, DropdownConfig};
 use super::editor::{Direction, DispatchEditor, IfCurrentNotFound};
-use super::editor_keymap::{alted, Meaning};
+use super::editor_keymap::alted;
 use super::keymap_legend::{Keybinding, Keymap};
 use super::{
     component::Component,
@@ -82,7 +82,7 @@ impl Component for SuggestiveEditor {
         event: event::KeyEvent,
     ) -> anyhow::Result<Dispatches> {
         if self.editor.mode == Mode::Insert && self.completion_dropdown_opened() {
-            if let Some(keymap) = completion_item_keymap(context).get(&event) {
+            if let Some(keymap) = completion_item_keymap().get(&event) {
                 log::info!("dispatches = {:?}", keymap.get_dispatches());
                 return Ok(keymap.get_dispatches());
             };
@@ -1164,34 +1164,22 @@ impl Decoration {
     }
 }
 
-pub fn completion_item_keymap(context: &Context) -> Keymap {
+pub fn completion_item_keymap() -> Keymap {
     Keymap::new(&[
         Keybinding::new_extended(
-            alted(
-                context
-                    .keyboard_layout_kind()
-                    .get_normal_keymap_keybinding(&Meaning::Right),
-            ),
+            alted("l"),
             Direction::End.format_action("Comp"),
             "Next Completion Item".to_string(),
             Dispatch::MoveToCompletionItem(Direction::End),
         ),
         Keybinding::new_extended(
-            alted(
-                context
-                    .keyboard_layout_kind()
-                    .get_normal_keymap_keybinding(&Meaning::Left_),
-            ),
+            alted("j"),
             Direction::Start.format_action("Comp"),
             "Previous Completion Item".to_string(),
             Dispatch::MoveToCompletionItem(Direction::Start),
         ),
         Keybinding::new_extended(
-            alted(
-                context
-                    .keyboard_layout_kind()
-                    .get_normal_keymap_keybinding(&Meaning::Cut__),
-            ),
+            alted("x"),
             "Replace Comp".to_string(),
             "Replace Completion Item".to_string(),
             Dispatch::SelectCompletionItem,

event/src/event.rs
@@ -168,7 +168,7 @@ pub enum KeyModifiers {
 }
 
 impl KeyModifiers {
-    pub(crate) fn add_shift(self, shift: bool) -> KeyModifiers {
+    pub(crate) fn add_shift(self, shift: bool) -> Self {
         use KeyModifiers::*;
         if !shift {
             return self;
@@ -180,6 +180,28 @@ impl KeyModifiers {
             CtrlAlt => CtrlAltShift,
             Unknown => Shift,
             _ => self,
+        }
+    }
+
+    pub(crate) fn remove_shift(self, shift: bool) -> Self {
+        use KeyModifiers::*;
+        if shift {
+            return self;
+        }
+        match self {
+            Shift => None,
+            CtrlShift => Ctrl,
+            AltShift => Alt,
+            CtrlAltShift => CtrlAlt,
+            _ => self,
+        }
+    }
+
+    pub fn set_shift(self, shift: bool) -> Self {
+        if shift {
+            self.add_shift(shift)
+        } else {
+            self.remove_shift(shift)
         }
     }
 

ki-vscode/src/managers/keyboard_manager.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+ll;
 import * as zlib from "node:zlib";
 import type { Dispatcher } from "../dispatcher";
 import type { Logger } from "../logger";

commit 8355e2529e2e44292dbf0c53afbf300702efcfac

fix(keymap): fix non-qwerty insert mode
Bug introduced by #1255.

src/app.rs
@@ -404,15 +404,6 @@ impl<T: Frontend> App<T> {
                 });
             }
             event => {
-                // Translate key events so that other layouts pretend to be QWERTY for the rest of the code
-                let event = match event {
-                    Event::Key(key_event) => Event::Key(
-                        self.context
-                            .keyboard_layout_kind()
-                            .translate_key_event_to_qwerty(key_event),
-                    ),
-                    event => event,
-                };
                 let dispatches = component.borrow_mut().handle_event(&self.context, event);
                 self.handle_dispatches_result(dispatches)
                     .unwrap_or_else(|e| {

src/components/editor.rs
@@ -1617,6 +1617,9 @@ impl Editor {
                         context,
                     )
                 } else {
+                    let key_event = context
+                        .keyboard_layout_kind()
+                        .translate_key_event_to_qwerty(key_event);
                     let keymap_legend_config = self.get_current_keymap_legend_config();
 
                     if let Some(keymap) = keymap_legend_config.keymap().get(&key_event) {

src/components/editor_keymap_legend.rs
@@ -581,10 +581,17 @@ impl Editor {
         context: &Context,
         event: KeyEvent,
     ) -> anyhow::Result<Dispatches> {
+        let translated_event = context
+            .keyboard_layout_kind()
+            .translate_key_event_to_qwerty(event.clone());
         if let Some(dispatches) = self
             .insert_mode_keymap(true)
             .iter()
-            .find(|keymap| keymap.event().is_press_or_repeat_equivalent(&event))
+            .find(|keymap| {
+                keymap
+                    .event()
+                    .is_press_or_repeat_equivalent(&translated_event)
+            })
             .map(|keymap| keymap.get_dispatches())
         {
             Ok(dispatches)

src/components/keymap_legend.rs
@@ -372,17 +372,20 @@ impl Component for KeymapLegend {
     ) -> Result<Dispatches, anyhow::Error> {
         let close_current_window = Dispatch::CloseCurrentWindowAndFocusParent;
         if self.editor.mode == Mode::Insert {
-            match &event {
+            match event {
                 key!("esc") => {
                     self.editor.enter_normal_mode(context)?;
                     Ok(Default::default())
                 }
                 key_event => {
+                    let key_event = context
+                        .keyboard_layout_kind()
+                        .translate_key_event_to_qwerty(key_event.clone());
                     if let Some(keymap) = self
                         .config
                         .keymap()
                         .iter()
-                        .find(|keymap| &keymap.event == key_event)
+                        .find(|keymap| keymap.event == key_event)
                     {
                         Ok(Dispatches::one(close_current_window)
                             .chain(keymap.get_dispatches())
@@ -414,7 +417,7 @@ impl Component for KeymapLegend {
                                     Dispatch::ShowKeymapLegend(keymap_legend_config.clone()),
                                 )),
                             }
-                        } else if &release_key.key_event == key_event {
+                        } else if release_key.key_event == key_event {
                             let on_tap_dispatches =
                                 match (&release_key.on_tap, release_key.other_keys_pressed) {
                                     (Some(on_tap), false) => on_tap.dispatches.clone(),

src/components/suggestive_editor.rs
@@ -82,11 +82,14 @@ impl Component for SuggestiveEditor {
         event: event::KeyEvent,
     ) -> anyhow::Result<Dispatches> {
         if self.editor.mode == Mode::Insert && self.completion_dropdown_opened() {
-            if let Some(keymap) = completion_item_keymap().get(&event) {
+            let translated_event = context
+                .keyboard_layout_kind()
+                .translate_key_event_to_qwerty(event.clone());
+            if let Some(keymap) = completion_item_keymap().get(&translated_event) {
                 log::info!("dispatches = {:?}", keymap.get_dispatches());
                 return Ok(keymap.get_dispatches());
             };
-            match event {
+            match translated_event {
                 key!("down") => return self.next_completion_item(),
                 key!("up") => return self.previous_completion_item(),
                 key!("tab") => return self.select_completion_item(),