ec7f219db4457a879226a2ac2cdea00a287a48a1
wongjiahau <hou32hou@gmail.com>
2026-03-24 10:30:33 +0800

    fix(logging): remove noisy entries from `notify::fsevent`
    ...which are fired whenever the file system is updated.

--- src/lib.rs
+++ src/lib.rs
@@ -93,7 +93,10 @@ fn init_logger() -> anyhow::Result<()> {
             tracing_subscriber::fmt::layer()
                 .with_writer(open_log_file(grammar::default_log_file())?)
                 .with_line_number(true)
-                .with_ansi(false),
+                .with_ansi(false)
+                .with_filter(tracing_subscriber::filter::filter_fn(|metadata| {
+                    !metadata.target().starts_with("notify::fsevent")
+                })),
         )
         .with(
             tracing_subscriber::fmt::layer()
