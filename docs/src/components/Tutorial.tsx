2c84294ac33f28065880adb46d9f83f8920c7e4c
wongjiahau <hou32hou@gmail.com>
2026-03-27 08:58:02 +0800

    fix(docs/Tutorial): shifted key renders as shift+undefined

--- docs/src/components/Tutorial.tsx
+++ docs/src/components/Tutorial.tsx
@@ -4,6 +4,12 @@ import { useEffect, useMemo, useState } from "react";
 import { type UseXTermProps, useXTerm } from "react-xtermjs";
 import * as z from "zod";
 import { useChosenKeyboardLayout } from "./Keymap";
+import {
+    KeyboardLayout,
+    keyboardLayoutSchema,
+    parseQwertyKey,
+    translateQwertyToTargetLayout,
+} from "./utils/parseQwertyKey";
 
 const recipeSchema = z.object({
     description: z.string(),
@@ -51,18 +57,11 @@ export const Tutorial = (props: { filename: string }) => {
     );
 };
 
-const keyboardLayoutSchema = z.object({
-    name: z.string(),
-    keys: z.array(z.array(z.string())),
-});
-
 export async function loadKeyboardLayouts(url: string) {
     const response = await fetch(url);
     const json = await response.json();
     return z.array(keyboardLayoutSchema).parse(json);
 }
-
-type KeyboardLayout = z.infer<typeof keyboardLayoutSchema>;
 
 const Recipe = (props: { recipe: Recipe }) => {
     const xtermOptions: UseXTermProps = useMemo(
@@ -299,39 +298,4 @@ const KeyboardHint = (props: {
             })}
         </div>
     );
-};
-
-const QWERTY_KEYS = [
-    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-    ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"],
-    ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"],
-];
-
-const translateQwertyToTargetLayout = (
-    qwertyKey: string,
-    targetLayout: KeyboardLayout,
-) => {
-    // We need to extract the key from the potentially added modifiers
-    const { keyboardModifier, releaseModifier, rowIndex, columnIndex } =
-        parseQwertyKey(qwertyKey);
-
-    const result = targetLayout.keys[rowIndex]?.[columnIndex ?? 0];
-    if (keyboardModifier && releaseModifier) {
-        return `${releaseModifier}-${keyboardModifier}+${result}`;
-    }
-    if (keyboardModifier) {
-        return `${keyboardModifier}+${result}`;
-    }
-    if (releaseModifier) {
-        return `${releaseModifier}-${result}`;
-    }
-    return result;
-};
-
-const parseQwertyKey = (raw: string) => {
-    const [rest, keyboardModifier] = raw.split("+").reverse();
-    const [key, releaseModifier] = (rest ?? "").split("-").reverse();
-    const rowIndex = QWERTY_KEYS.findIndex((row) => row.includes(key ?? ""));
-    const columnIndex = QWERTY_KEYS[rowIndex]?.indexOf(key ?? "");
-    return { keyboardModifier, releaseModifier, rowIndex, columnIndex };
 };
