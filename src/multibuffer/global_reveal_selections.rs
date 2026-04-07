7fa45dbdacfa0cd15da6c7329281f95d4d0ddcfd
wongjiahau <hou32hou@gmail.com>
2026-04-07 12:45:54 +0800

    move global reveal related code to its own file

--- src/multibuffer/global_reveal_selections.rs
+++ src/multibuffer/global_reveal_selections.rs
@@ -1,7 +1,14 @@
 #[cfg(test)]
 use crate::components::suggestive_editor::SuggestiveEditor;
-use crate::multibuffer::MultibufferFile;
-#[cfg(test)]
+use crate::{
+    app::App,
+    buffer::BufferOwner,
+    components::editor::{DispatchEditor, Reveal},
+    context::GlobalMode,
+    frontend::Frontend,
+    multibuffer::{Multibuffer, MultibufferFile},
+};
+
 use itertools::Itertools;
 #[cfg(test)]
 use std::{cell::RefCell, rc::Rc};
@@ -18,5 +25,53 @@ impl GlobalRevealSelections {
             .iter()
             .map(|file| file.editor.clone())
             .collect_vec()
+    }
+}
+impl<T: Frontend> App<T> {
+    pub fn toggle_reveal_selections(&mut self) -> anyhow::Result<()> {
+        if self.context.mode() == Some(GlobalMode::QuickfixListItem) {
+            if self.multibuffer.is_some() {
+                self.multibuffer = None;
+                Ok(())
+            } else {
+                self.active_global_reveal_selections()
+            }
+        } else {
+            self.handle_dispatch_editor(DispatchEditor::ToggleReveal(Reveal::CurrentSelectionMode))
+        }
+    }
+
+    fn active_global_reveal_selections(&mut self) -> anyhow::Result<()> {
+        let paths = self
+            .quickfix_list()
+            .items()
+            .iter()
+            .map(|item| item.location().path.clone())
+            .unique()
+            .sorted()
+            .collect_vec();
+
+        let files = paths
+            .into_iter()
+            .map(|path| -> anyhow::Result<_> {
+                let editor = self.open_file(&path, BufferOwner::User, true, false)?;
+                Ok(MultibufferFile { path, editor })
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
+
+        if !files.is_empty() {
+            self.multibuffer = Some(Multibuffer::GlobalRevealSelections(
+                GlobalRevealSelections {
+                    // We'll just assume the first file is the focused file, for simplicity purposes
+                    files,
+                    focused_file_index: 0,
+                },
+            ));
+
+            // Close the quickfix list
+            self.layout.remain_only_current_component();
+        }
+
+        Ok(())
     }
 }
--- src/multibuffer/mod.rs
+++ src/multibuffer/mod.rs
@@ -1,14 +1,12 @@
 use crate::app::{App, Dimension};
-use crate::context::GlobalMode;
 mod global_multicursor;
 mod global_reveal_selections;
 use self::global_multicursor::GlobalMulticursor;
 #[cfg(test)]
 mod test_global_multicursor;
 use self::global_reveal_selections::GlobalRevealSelections;
-use crate::buffer::BufferOwner;
 use crate::components::component::{Component, Cursor, RenderTitleMode, SetCursorStyle};
-use crate::components::editor::{DispatchEditor, Reveal};
+use crate::components::editor::Reveal;
 use crate::components::suggestive_editor::SuggestiveEditor;
 use crate::divide_viewport::divide_viewport;
 use crate::grid::Grid;
@@ -67,53 +65,6 @@ pub struct MultibufferFile {
 }
 
 impl<T: Frontend> App<T> {
-    pub fn toggle_reveal_selections(&mut self) -> anyhow::Result<()> {
-        if self.context.mode() == Some(GlobalMode::QuickfixListItem) {
-            if self.multibuffer.is_some() {
-                self.multibuffer = None;
-                Ok(())
-            } else {
-                self.active_global_reveal_selections()
-            }
-        } else {
-            self.handle_dispatch_editor(DispatchEditor::ToggleReveal(Reveal::CurrentSelectionMode))
-        }
-    }
-
-    fn active_global_reveal_selections(&mut self) -> anyhow::Result<()> {
-        let paths = self
-            .quickfix_list()
-            .items()
-            .iter()
-            .map(|item| item.location().path.clone())
-            .unique()
-            .sorted()
-            .collect_vec();
-
-        let files = paths
-            .into_iter()
-            .map(|path| -> anyhow::Result<_> {
-                let editor = self.open_file(&path, BufferOwner::User, true, false)?;
-                Ok(MultibufferFile { path, editor })
-            })
-            .collect::<anyhow::Result<Vec<_>>>()?;
-
-        if !files.is_empty() {
-            self.multibuffer = Some(Multibuffer::GlobalRevealSelections(
-                GlobalRevealSelections {
-                    // We'll just assume the first file is the focused file, for simplicity purposes
-                    files,
-                    focused_file_index: 0,
-                },
-            ));
-
-            // Close the quickfix list
-            self.layout.remain_only_current_component();
-        }
-
-        Ok(())
-    }
-
     pub fn render_multibuffer(
         &self,
         multibuffer: &Multibuffer,
