b7e2fd5784a314f9c9c6bded41218f2b0137b6d6
wongjiahau <hou32hou@gmail.com>
2026-05-02 12:35:07 +0800

    fix format

--- src/test_app.rs
+++ src/test_app.rs
@@ -3195,9 +3195,7 @@ fn pasting_when_clipboard_html_is_set_by_other_app() -> Result<(), anyhow::Error
                     owner: BufferOwner::User,
                     focus: true,
                 }),
-                App(Dispatch::SetSystemClipboardContent {
-                    content: "hello",
-                }),
+                App(Dispatch::SetSystemClipboardContent { content: "hello" }),
                 Editor(SetSelectionMode(IfCurrentNotFound::LookForward, Character)),
                 Editor(SetContent("".to_string())),
                 Editor(PasteWithMovement(GetGapMovement::Right)),
